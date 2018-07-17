1b54b14eb4b27e0b942eb2ac607ea83d341d384c
Merlin Webster <mwebster@witekio.com>
2018-07-17 10:19:11 +0100

    farmclass: Update to show_hier()

--- farmclass.py
+++ farmclass.py
@@ -1,7 +1,20 @@
 class Farmclass():
     """ Contains functionality common to all farm objects """
+
     def log(self, message):
         print(message)
+
+    def _get_hier(self):
+        farmobjs = {}
+        attrs = {}
+        for m in dir(self):
+            member = getattr(self, m)
+            if isinstance(member, Farmclass):
+                farmobjs[m] = member
+            elif(not m.startswith("_") and
+                    not callable(member)):
+                attrs[m] = member
+        return farmobjs, attrs
 
     def show_hier(self, indent_level=1, indent_size=4):
         """ Print all local vars, and get child farm objects to do the same """
@@ -16,13 +29,8 @@ class Farmclass():
         print("-"*type_indent_level*indent_size +
               "{}:".format(type(self).__name__))
 
-        for m in dir(self):
-            member = getattr(self, m)
-            if isinstance(member, Farmclass):
-                farmobjs[m] = member
-            elif(not m.startswith("_") and
-                    not callable(member)):
-                attrs[m] = member
+        farmobjs, attrs = self._get_hier()
+
         for key in attrs:
             print("-"*indent_level*indent_size +
                   "{}: {}".format(key, attrs[key]))
