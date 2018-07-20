214cccbd1cb87db19f9d9594538e4704b79abf64
Merlin Webster <mwebster@witekio.com>
2018-07-20 11:25:39 +0100

    Farmclass: show_hier() now returns a string, added __repr__ function

--- farmclass.py
+++ farmclass.py
@@ -4,6 +4,9 @@ class Farmclass():
     def log(self, message):
         print(message)
 
+    def __repr__(self):
+        return self.show_hier(reccurse=False)
+
     def _get_hier(self):
         farmobjs = {}
         attrs = {}
@@ -16,23 +19,30 @@ class Farmclass():
                 attrs[m] = member
         return farmobjs, attrs
 
-    def show_hier(self, indent_level=1, indent_size=4):
+    def show_hier(self, indent_level=1, indent_size=4, reccurse=True):
         """ Print all local vars, and get child farm objects to do the same """
         farmobjs = {}
         attrs = {}
+        hier_str = ''
 
         if(indent_level > 0):
             type_indent_level = indent_level - 1
         else:
             type_indent_level = indent_level
 
-        print("-"*type_indent_level*indent_size +
-              "{}:".format(type(self).__name__))
+        hier_str += ("-"*type_indent_level*indent_size +
+            "{}:\n".format(type(self).__name__))
 
         farmobjs, attrs = self._get_hier()
 
         for key in attrs:
-            print("-"*indent_level*indent_size +
-                  "{}: {}".format(key, attrs[key]))
+            hier_str += ("-"*indent_level*indent_size +
+                  "{}: {}\n".format(key, attrs[key]))
         for key in farmobjs:
-            farmobjs[key].show_hier(indent_level + 1, indent_size)
+            if reccurse:
+                hier_str += farmobjs[key].show_hier(indent_level + 1, indent_size)
+            else:
+                hier_str += ("-"*indent_level*indent_size +
+              "{}: {}\n".format(key, type(farmobjs[key]).__name__))
+
+        return hier_str
