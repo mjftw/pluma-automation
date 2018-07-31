835cf5f83c06c0b5726b84140e79c7ce384d50da
Merlin Webster <mwebster@witekio.com>
2018-07-31 16:37:23 +0100

    WUKF-17: usb: Adding in USB class code from mwebster-dev branch

--- farmcore/usb.py
+++ farmcore/usb.py
@@ -3,12 +3,11 @@
 import sys
 import os
 import time
-from pprint import pprint as pp
-import subprocess as sp
-import pyudev as pu
+import subprocess
 
-bind_path = "/sys/bus/usb/drivers/usb/bind"
-unbind_path = "/sys/bus/usb/drivers/usb/unbind"
+import pyudev
+
+driver_path = '/sys/bus/usb/drivers/usb/'
 
 
 class NoPartitions(Exception):
@@ -23,87 +22,81 @@ class NoDevice(Exception):
     pass
 
 
-class usb():
-    def __init__(self):
-        self.puc = pu.Context()
+class USB():
+    def __init__(self, device):
+        self.puctx = pyudev.Context()
+        self.device = device
 
-    def unbind(self, usb_device):
-        try:
-            #print("unbinding {}".format( usb_device ) )
-            device = self.get_device(usb_device)
-            if device is None:
-                return
-            d = os.path.basename(device['DEVPATH'])
-            unbind_fd = open(unbind_path, 'w')
-            unbind_fd.write(d)
-            unbind_fd.close()
-        except:
-            #print("No device to unbind, lets carry on")
-            pass
+    @property
+    def is_bound(self):
+        return os.path.isdir(os.path.join(driver_path, self.device))
+
+    def unbind(self):
+        if not self.is_bound:
+            return
+        with open(os.path.join(driver_path, 'unbind'), 'w') as fd:
+            fd.write(self.device)
         time.sleep(1)
 
-    def bind(self, usb_device):
-        #print("binding {}".format( usb_device ) )
-        device = self.get_device(usb_device)
-        d = os.path.basename(device['DEVPATH'])
-        bind_fd = open(bind_path, 'w')
-        bind_fd.write(d)
-        bind_fd.close()
+    def bind(self):
+        if self.is_bound:
+            return
+        with open(os.path.join(driver_path, 'bind'), 'w') as fd:
+            fd.write(self.device)
         time.sleep(1)
 
-    def rebind(self, usb_device):
-        self.unbind(usb_device)
-        self.bind(usb_device)
+    def rebind(self):
+        self.unbind()
+        self.bind()
 
-    def get_device(self, usb_device):
-        #print("Looking for device {}".format( usb_device ))
+    def get_device(self):
         try:
             for _ in range(20):
-                d = pu.Devices.from_path(
-                    self.puc, '/bus/usb/devices/{}'.format(usb_device))
+                d = pyudev.Devices.from_path(
+                    self.puctx, '/bus/usb/devices/{}'.format(self.device))
                 if d is not None:
                     break
                 time.sleep(1)
         except:
-            raise NoDevice("No device for [{}]".format(usb_device))
+            raise NoDevice("No device for [{}]".format(self.device))
         return d
 
-    def get_serial(self, usb_device):
-        d = self.get_device(usb_device)
+    def get_serial(self):
+        d = self.get_device()
         if d is None:
             return None
         #print("Searching for serial devicces on [{}]".format( usb_device ))
         for _ in range(20):
-            for m in self.puc.list_devices(subsystem='tty', parent=d):
+            for m in self.puctx.list_devices(subsystem='tty', parent=d):
                 #print("Found serial {}".format( m.device_node ))
                 return m.device_node
             time.sleep(2)
-        raise IOError('No serial devices on '.format(usb_device))
+        raise IOError('No serial devices on '.format())
 
-    def get_sdmux(self, usb_device):
+    def get_sdmux(self):
         sdmux_id = 'DLP_Design'
-        d = self.get_device(usb_device)
+        d = self.get_device()
         if d is None:
             return None
-        #print("Searching for sdmux devicces on [{}]".format( usb_device ))
+        #print("Searching for sdmux devicces on [{}]".format( self.device ))
         for _ in range(20):
-            for m in self.puc.list_devices(subsystem='tty',  parent=d):
+            for m in self.puctx.list_devices(subsystem='tty',  parent=d):
                 if m.get('ID_VENDOR') is None:
                     continue
                 if m['ID_VENDOR'] == sdmux_id:
                     #print("Found sdmux as {}".format( m.device_node ))
                     return m.device_node
             time.sleep(2)
-        raise IOError('No sdmux devices on '.format(usb_device))
+        raise IOError('No sdmux devices on {}'.format(self.device))
 
     # Return the device node and the sysname for the usb parent for use with the bind/unbind
-    def get_block(self, usb_device):
-        d = self.get_device(usb_device)
+    def get_block(self):
+        d = self.get_device()
         if d is None:
             return None
-        #print("Searching for block devicces on [{}]".format( usb_device ))
+        #print("Searching for block devicces on [{}]".format( self.device ))
         for _ in range(20):
-            blks = self.puc.list_devices(
+            blks = self.puctx.list_devices(
                 subsystem='block',  DEVTYPE='disk', parent=d)
             for m in blks:
                 if int(m.attributes.get('size')) == 0:
@@ -115,16 +108,16 @@ class usb():
                 #print(" Parent is: {} , {}".format( usbp.sys_name, usbp.device_type ))
                 return(m.device_node, usbp.sys_name)
             time.sleep(2)
-        raise NoDisks('No block devices on {}'.format(usb_device))
+        raise NoDisks('No block devices on {}'.format(self.device))
 
     # Get first partition name for block device
-    def get_part(self, usb_device):
-        d = self.get_device(usb_device)
+    def get_part(self):
+        d = self.get_device()
         if d is None:
-            raise NoDevice("No device for {}".format(usb_device))
-        #print("Searching for block devicces on [{}]".format( usb_device ))
+            raise NoDevice("No device for {}".format(self.device))
+        #print("Searching for block devicces on [{}]".format( self.device ))
         for _ in range(5):
-            blks = self.puc.list_devices(
+            blks = self.puctx.list_devices(
                 subsystem='block',  DEVTYPE='partition', parent=d)
             for m in blks:
                 if int(m.attributes.get('size')) == 0: continue
@@ -135,22 +128,22 @@ class usb():
                 #print(" Parent is: {} , {}".format( usbp.sys_name, usbp.device_type ))
                 return m.device_node
             time.sleep(2)
-        raise NoPartitions('No partitions on {}'.format(usb_device))
+        raise NoPartitions('No partitions on {}'.format(self.device))
 
-    def show_ancestry(self, usb_device):
-        d = self.get_device(usb_device)
-        if d is None:
+    def show_ancestry(self):
+        dev = self.get_device()
+        if dev is None:
             return None
 
-        while d.parent:
-            p = d.parent
+        while dev.parent:
+            p = dev.parent
             print(dir(p))
             print(p.driver, p.subsystem, p.sys_name, p.device_path, p.sys_path)
-            sp.run(['ls', "{}/driver".format(p.sys_path)])
-            d = p
+            subprocess.run(['ls', "{}/driver".format(p.sys_path)])
+            dev = p
 
-    def rebind_host(self, usb_device):
-        d = self.get_device(usb_device)
+    def rebind_host(self):
+        d = self.get_device()
         if d is None:
             return None
 
@@ -163,8 +156,8 @@ class usb():
 
         print("WARN: Rebinding HOST {} at {}".format(d.sys_name, driver_path))
 
-        sp.run(['sudo', 'chmod', 'a+w', driver_path + "/unbind"])
-        sp.run(['sudo', 'chmod', 'a+w', driver_path + "/bind"])
+        subprocess.run(['sudo', 'chmod', 'a+w', driver_path + "/unbind"])
+        subprocess.run(['sudo', 'chmod', 'a+w', driver_path + "/bind"])
 
         with open("{}/unbind".format(driver_path), 'w') as f:
             f.write(d.sys_name)
@@ -174,9 +167,5 @@ class usb():
             f.write(d.sys_name)
 
 
-    #find_block( usb_device )
-    #find_serial( usb_device )
-
-
-if __name__ == "__main__":
-    usb().show_info()
+    #find_block()
+    #find_serial()
