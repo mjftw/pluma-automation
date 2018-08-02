84015ec682bb8703aad233b165b30ad9e1f2c2ca
Merlin Webster <mwebster@witekio.com>
2018-08-02 10:12:56 +0100

    WUKF-17: usb_relay: Refactored to use USB base class

--- farmcore/usb_relay.py
+++ farmcore/usb_relay.py
@@ -1,32 +1,20 @@
 
 import serial
 import time
+
 from farmcore.farm_base import FarmBase
-from farmcore import usb
+from farmcore.usb import USB
 
 # There are USB relays which have four channels 0,1,2,3
 # The SDMUXes are connected to them
-# The SDMUCes are also powered by the APC unit.
+# The SDMUXes are also powered by the APC unit.
 
-class USBRelay(FarmBase):
-    def __init__(self, usbpath):
-        self.usbpath = usbpath
-        self.usb = usb.usb()
-        self._s = None
-        sdmux_id = 'DLP_Design'
-        d = self.usb.get_device( self.usbpath )
-        if d is None:
-            return None
-        for _ in range(20):
-            for m in self.usb.puc.list_devices(subsystem='tty',  parent=d):
-                if m.get('ID_VENDOR') is None:
-                    continue
-                if m['ID_VENDOR'] == sdmux_id:
-                    self.devnode = m.device_node
-                    self.log("Registered USBRelay at {}".format( self.devnode )) 
-                    return
-            time.sleep(2)
-        raise IOError('No usb-relay devices on '.format(usb_device))
+
+class USBRelay(FarmBase, USB):
+    def __init__(self, usb_device):
+        USB.__init__(usb_device)
+        self.devnode = self.get_sdmux()
+        self.log("Registered USBRelay at {}".format(self.devnode))
 
     @property
     def s(self):
@@ -37,9 +25,8 @@ class USBRelay(FarmBase):
     def write(self, data):
         return self.s.write(data)
 
-    def __getitem__(self,key):
-        return (self,key)
-
+    def __getitem__(self, key):
+        return (self, key)
 
     def __repr__(self):
-        return "USBRelay[{}]".format( self.usbpath )
+        return "USBRelay[{}]".format(self.usb_device)
