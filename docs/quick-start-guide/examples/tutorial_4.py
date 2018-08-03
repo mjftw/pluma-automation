fc45671d9d811fa31dc5a3aae3d9fff45df5cac9
Merlin Webster <mwebster@witekio.com>
2018-08-03 14:09:13 +0100

    qsg: changed commenting style of tutorial 1,4,5,6 to match that of tutorial 3

--- docs/quick-start-guide/examples/tutorial_4.py
+++ docs/quick-start-guide/examples/tutorial_4.py
@@ -1,8 +1,5 @@
-"""
-Tutorial example 4:
-    - Find a boards prompt
-    - Send a command over serial
-    - Wait for the command to finish exection
-    - Print the result
-"""
-
+# Tutorial example 4:
+#     - Find a boards prompt
+#     - Send a command over serial
+#     - Wait for the command to finish exection
+#     - Print the result
--- docs/quick-start-guide/examples/tutorial_5.py
+++ docs/quick-start-guide/examples/tutorial_5.py
@@ -1,7 +1,5 @@
-"""
-Tutorial example 5:
-    - Switch a boards's SD card to the farm host
-    - Mount the SD card and copy a file to it
-    - Switch SD card back to the board
-    - Print the contents of the file over serial
-"""
+# Tutorial example 5:
+#     - Switch a boards's SD card to the farm host
+#     - Mount the SD card and copy a file to it
+#     - Switch SD card back to the board
+#     - Print the contents of the file over serial
