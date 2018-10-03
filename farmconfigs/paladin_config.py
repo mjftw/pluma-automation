86c8722ce5c215f0413cbf5e70039e6732132425
Merlin Webster <mwebster@witekio.com>
2018-10-03 21:49:40 +0100

    Updated format of paladin test script

--- farmconfigs/paladin_config.py
+++
@@ -1,4 +0,0 @@
-from farmcore import farm
-
-relay = farm.USBRelay('3-3.3')
-paladin = farm.Board('paladin', None, '3-3.2', None)
--- paladin.py
+++ paladin.py
@@ -1,35 +1,23 @@
 import time
 
-from farmcore.usb_relay import USBRelay
-# import farmcore.interact
-# from farmcore.board import Board
+from farmcore import farm
 
-relay = USBRelay('/dev/ttyUSB1')
-# paladin = Board('paladin', None, '3-3.2', None)
 
-def power(state):
-    if state not in ['on', 'off']:
-        print("Error: power must be 'on' or 'off'")
-
-    if state == 'on':
-        print("Power on")
-        relay.toggle(1, 'A')
-        relay.toggle(2, 'B')
-        time.sleep(0.1)
-
-        relay.toggle(2, 'A')
-        time.sleep(0.1)
-        relay.toggle(2, 'B')
-
-    if state == 'off':
-        print("Power off")
-        relay.toggle(1, 'B')
-        relay.toggle(2, 'B')
 
 def main():
-    power('off')
-    time.sleep(1)
-    power('on')
+    paladin = farm.Board(
+        name='paladin',
+        power=farm.PowerRelay('3-3.3'),
+        hub=farm.Hub('3-3'),
+        sdmux=None,
+        logfile="paladin.log"
+        # logfile=None
+    )
+
+    paladin.power.off()
+    paladin.power.on()
+    paladin.console.wait_for_quiet(timeout=20, quiet=5)
+
 
 if __name__ == '__main__':
     main()
