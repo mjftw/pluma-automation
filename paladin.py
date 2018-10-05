539b46b38894712d533b68d8ce1f242133cc65a4
Merlin Webster <mwebster@witekio.com>
2018-10-05 19:00:29 +0100

    Added build email sending. Added tests within hours

--- paladin.py
+++ paladin.py
@@ -1,6 +1,9 @@
 import time
+import datetime
+
 
 from farmcore import farm
+from farmutils.doemail import Email
 
 
 def main():
@@ -22,6 +25,30 @@ def main():
 
     boot_test(paladin, 10000)
 
+
+def time_in_range(start_hour, end_hour):
+    now = datetime.datetime.now()
+    return (start_hour <= now.hour and now.hour < end_hour)
+
+
+def send_test_report(board):
+    board.log("Sending test report")
+
+    body = """ --------- Automated Paladin boot test report ---------
+               Boot attempts: {}, Pass: {}, Fail: {}""".format(
+                   test_no, pass_no, fail_no
+               )
+    Email(
+       sender="mwebster@witekio.com",
+       to="mwebster@witekio.com",
+       cc=["probinson@witekio.com", "elangley@witekio.com", "asison@witekio.com"],
+       subject="Automated Paladin boot test report",
+       body=body,
+       files=[board.logfile],
+       smtp_authfile='./smtp.auth'
+    ).send()
+
+
 def boot_test(board, itterations):
 
     boot_string = "SoloProtect"
@@ -34,22 +61,31 @@ def boot_test(board, itterations):
         itterations, boot_string)
     )
 
-    while test_no < itterations:
-        test_no += 1
-        board.power.restart()
-        try:
-            board.console.send("", True, boot_string)
-            pass_no += 1
+    start_hour = 6
+    stop_hour = 18
 
-        except farm.TimeoutNoRecieve:
-            board.log("TIMEOUT waiting for boot string...")
-            fail_no += 1
+    while True:
+        while not time_in_range(start_hour, stop_hour):
+            test_no += 1
+            board.power.restart()
+            try:
+                board.console.send("", True, boot_string)
+                pass_no += 1
 
-        board.log("Test #: {}, pass #: {}, fail #: {}".format(
-            test_no, pass_no, fail_no)
-        )
+            except farm.TimeoutNoRecieve:
+                board.log("TIMEOUT waiting for boot string...")
+                fail_no += 1
 
-    board.log("---- Finished tests ----")
+            board.log("Test #: {}, pass #: {}, fail #: {}".format(
+                test_no, pass_no, fail_no)
+            )
+
+        send_test_report(board)
+
+        while time_in_range(start_hour, stop_hour):
+            time.sleep(600)
+
+
 
 if __name__ == '__main__':
     main()
