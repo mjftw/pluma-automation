8d03d7bd37386ec3fc8c00e4c576c5a2d7df5893
Merlin Webster <mwebster@witekio.com>
2018-10-17 09:44:19 +0100

    farmclass: Large update to how logging works in farm base class 	- Switched to using properties, removing the need for 	  a method to have to be called in order to update the 	  logging settigns for the child farm objects. 	- BUG: Seems to be a minor bug where the no more than 2 levels 	  of hierachy can be stored in the hierachy path string 	  "log_hier_path". 	- Added a new class FarmobjMock which can be used to sandbox 	  how to hierachial logging system works.

--- farmcore/farmclass.py
+++ farmcore/farmclass.py
@@ -1,32 +1,130 @@
 import datetime
 import os
 
-
+DEFAULT_LOG_TIMEFORMAT = "%y-%m-%d %H:%M:%S"
 class Farmclass():
     """ Contains functionality common to all farm objects """
 
     def __repr__(self):
         return self.show_hier(reccurse=False)
 
+    @property
+    def log_on(self):
+        if hasattr(self, "_log_on"):
+            return self._log_on
+        else:
+            return False
+
+    @log_on.setter
+    def log_on(self, log_on):
+        self._log_on = log_on
+        if self.log_reccurse:
+            self._children_set_attr("log_on", log_on)
+
+    @property
+    def log_name(self):
+        if hasattr(self, "_log_name"):
+            return self._log_name
+        else:
+            return None
+
+    @log_name.setter
+    def log_name(self, log_name):
+        self._log_name = log_name
+        if self.log_reccurse:
+            self._children_set_attr("log_name", log_name)
+
+    @property
+    def log_time(self):
+        if hasattr(self, "_log_time"):
+            return self._log_time
+        else:
+            return False
+
+    @log_time.setter
+    def log_time(self, log_time):
+        self._log_time = log_time
+        if self.log_reccurse:
+            self._children_set_attr("log_time", log_time)
+
+    @property
+    def log_file(self):
+        if hasattr(self, "_log_file"):
+            return self._log_file
+        else:
+            return None
+
+    @log_file.setter
+    def log_file(self, log_file):
+        self._log_file = log_file
+        if self.log_reccurse:
+            self._children_set_attr("log_file", log_file)
+
+    @property
+    def log_echo(self):
+        if hasattr(self, "_log_echo"):
+            return self._log_echo
+        else:
+            return False
+
+    @log_echo.setter
+    def log_echo(self, log_echo):
+        self._log_echo = log_echo
+        if self.log_reccurse:
+            self._children_set_attr("log_echo", log_echo)
+
+    @property
+    def log_hier_path(self):
+        if hasattr(self, "_log_hier_path"):
+            return self._log_hier_path
+        else:
+            return False
+
+    @log_hier_path.setter
+    def log_hier_path(self, log_hier_path):
+        if log_hier_path:
+            if isinstance(log_hier_path, bool):
+                self._log_hier_path = '{}'.format(type(self).__name__)
+            else:
+                self._log_hier_path = '{}.{}'.format(
+                        log_hier_path, type(self).__name__)
+        else:
+            self._log_hier_path = False
+
+        if self.log_reccurse:
+            self._children_set_attr("log_hier_path", self.log_hier_path)
+
+    @property
+    def log_reccurse(self):
+        if hasattr(self, "_log_reccurse"):
+            return self._log_reccurse
+        else:
+            return False
+
+    @log_reccurse.setter
+    def log_reccurse(self, log_reccurse):
+        self._log_reccurse = log_reccurse
+        self._children_set_attr("log_reccurse", log_reccurse)
+
     def log(self, message):
         prefix = ''
-        if hasattr(self, 'logon') and self.logon is True:
-            if hasattr(self, '_appendtype') and self._appendtype:
-                prefix = '[{}]{}'.format(self._appendtype, prefix)
-            if hasattr(self, 'logname') and self.logname:
-                prefix = '{}{}'.format(self.logname, prefix)
-            if hasattr(self, 'logtime') and self.logtime:
+        if self.log_on:
+            if self.log_hier_path:
+                prefix = '[{}]{}'.format(self.log_hier_path, prefix)
+            if self.log_name:
+                prefix = '{}{}'.format(self.log_name, prefix)
+            if self.log_time:
                 timestr = datetime.datetime.now().strftime("%y-%m-%d %H:%M:%S")
                 prefix = '{} {}'.format(timestr, prefix)
-            if prefix != '':
+            if prefix:
                 message = '{}: {}'.format(prefix, message)
-            if hasattr(self, 'logfile') and self.logfile:
-                logdir = os.path.dirname(self.logfile)
+            if self.log_file:
+                logdir = os.path.dirname(self.log_file)
                 if logdir and not os.path.exists(logdir):
                     os.makedirs(logdir)
-                with open(self.logfile, 'a') as logfd:
+                with open(self.log_file, 'a') as logfd:
                     logfd.write(message + '\n')
-            else:
+            if self.log_echo:
                 print(message)
 
     def _get_hier(self):
@@ -41,30 +139,12 @@ class Farmclass():
                 attrs[m] = member
         return farmobjs, attrs
 
-    def set_logger(self, logname=None, logfile=None,
-                   appendtype=None, logtime=False, reccurse=False,
-                   logon=True):
-        self.logon = logon
-        self.logfile = logfile
-        self.logname = logname
-        self.logtime = logtime
-        if appendtype:
-            if isinstance(appendtype, bool):
-                self._appendtype = '{}'.format(type(self).__name__)
-            else:
-                self._appendtype = '{}.{}'.format(
-                        appendtype, type(self).__name__)
-
-        if reccurse:
-            farmobjs, __ = self._get_hier()
-            for key in farmobjs:
-                farmobjs[key].set_logger(
-                    logname=self.logname,
-                    logfile=logfile,
-                    appendtype=self._appendtype,
-                    logtime=self.logtime,
-                    reccurse=reccurse,
-                )
+    def _children_set_attr(self, attr, value):
+        farmobjs, __ = self._get_hier()
+        for key in farmobjs:
+            child = farmobjs[key]
+            setattr(child, attr, value)
+            child._children_set_attr(attr, value)
 
     def show_hier(self, indent_level=1, indent_size=4, reccurse=True):
         """ Print all local vars, and get child farm objects to do the same """
---
+++ farmcore/farmobj_mock.py
@@ -0,0 +1,14 @@
+from .farmclass import Farmclass
+
+class FarmobjMock(Farmclass):
+    def __init__(self, children=None):
+        self.num_children = 0
+        if isinstance(children, list):
+            for child in children:
+                self.add_child(child)
+        if isinstance(children, Farmclass):
+            self.add_child(children)
+
+    def add_child(self, child):
+        setattr(self, "child_{}".format(self.num_children), child)
+        self.num_children += 1
