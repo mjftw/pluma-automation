3f029e7ffd11d0f6faf177f5964829fc45f30ffc
Merlin Webster <mwebster@witekio.com>
2018-10-22 03:32:25 +0100

    Paladin: Updated to use new version of test framework

--- paladin.py
+++ paladin.py
@@ -4,8 +4,7 @@ import os
 
 from farmcore import farm
 from farmutils.doemail import Email
-from farmutils.test import TestSuite, Test, testlib
-
+from farmutils.test import TestSuite, Test, test_func, testlib
 
 def main():
     hub = farm.Hub('1-3')
@@ -14,17 +13,15 @@ def main():
         power=farm.PowerRelay(
             hub=hub,
             on_seq=[(1, 'A'), (2, 'B'), "100ms", (2, 'A'), "100ms", (2, 'B')],
-            off_seq=[(1, 'B'), (2, 'B'), "5000ms"]
+            off_seq=[(1, 'B'), (2, 'B'), "5s"]
         ),
         hub=hub,
         sdmux=None,
         logfile="paladin.log"
-        #logfile=None
     )
     paladin.console.logon = False
     paladin.power.logon = True
 
-    iterations = 1000
     boot_string = "login"
 
     # When running on a beaglebone the TZ is not set, so these are UTC times and you need to take account of any DST adjustment.
@@ -32,17 +29,21 @@ def main():
     stop_hour = 10
 
     suite = TestSuite(
-        setup_func=testlib.ss_boot(paladin, boot_string, iterations),
-        tests=Test(
-            fbody=testlib.tb_boot(paladin, boot_string)
-        ),
-        report_func=(send_test_report, paladin),
+        name="Paladin"
+        setup_func=testlib.ss_boot(paladin, boot_string),
+        tests=[
+            testlib.tb_boot(paladin, boot_string),
+            testlib.tb_login(paladin, user="root", prompt="#")
+        ],
+        report_func=send_test_report(paladin),
         run_condition_func=testlib.sc_time_in_range(9, 10),
-        run_forever=True
+        run_forever=True,
     )
 
     suite.run()
 
+
+@test_func
 def send_test_report(suite, board):
     board.log("Sending test report")
 
