5df11ff1fc8d918e4620025ced386e9d48f0560d
Merlin Webster <mwebster@witekio.com>
2018-11-22 15:39:07 +0000

    Added driver class for Tizen SDWire

---
+++ farmcore/sdwire.py
@@ -0,0 +1,32 @@
+from pyftdi.ftdi import Ftdi
+
+from .farmclass import Farmclass
+
+
+class SDWire(Farmclass):
+    def __init__(self, serial=None):
+        self.serial = serial
+        self.ftdi = Ftdi()
+
+        self.vendor_id = 0x04e8
+        self.product_id = 0x6001
+
+    def _open(self):
+        self.ftdi.open(
+            vendor=self.vendor_id,
+            product=self.product_id,
+            serial=self.serial
+        )
+
+    def _close(self):
+        self.ftdi.close()
+
+    def to_host(self):
+        self._open()
+        self.ftdi.set_bitmode(mode=0x20, bitmask=0xf0)
+        self._close()
+
+    def to_board(self):
+        self._open()
+        self.ftdi.set_bitmode(mode=0x20, bitmask=0xf1)
+        self._close()
