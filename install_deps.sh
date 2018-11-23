a888024d8ab695ca8aec30d458c27cdf08446535
Merlin Webster <mwebster@witekio.com>
2018-11-23 17:08:58 +0000

    Added pyftdi to requirements, and added install_deps script

---
+++ install_deps.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Dependency installer script for Debian based Linux distributions
+#
+
+# LibUSB required for PyFTDI library used by SDWire
+sudo apt install -y libusb-1.0 python3 python3-pip
+
+# Install Python libraries
+pip3 install -r requirements.txt
