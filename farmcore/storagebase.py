938ab3602f25ed636e85256887b087096125de4d
Merlin Webster <mwebster@witekio.com>
2018-11-30 10:54:04 +0000

    StorageBase: Now handles mounting and unmounting storage on host

--- farmcore/storagebase.py
+++ farmcore/storagebase.py
@@ -1,8 +1,17 @@
+import subprocess
+import re
+
+class StorageException(Exception):
+    pass
+
+
 class StorageBase():
     '''
     Base class for storage classes that can be switched between
     the host and board.
     '''
+
+    host_mountpoint = None
 
     def to_host(self):
         ''' Switch storage to the host '''
@@ -12,19 +21,47 @@ class StorageBase():
         ''' Switch storage to the board '''
         raise NotImplemented('This method must be implimented by inheriting class')
 
+    def mount_host(self, devnode, mountpoint=None):
+        mountpoint = mountpoint or self.host_mountpoint
+        if not mountpoint:
+            raise StorageException('Mount failed: No mountpoint given')
+
+        # Check if already mounted
+        (output, ret) = self._host_cmd('mount')
+
+        # Unmount if mounted
+        if re.search('{}.*'.format(devnode), output):
+            self.unmount_host(devnode)
+
+        (output, ret) = self._host_cmd('mount {} {}'.format(devnode, mountpoint))
+        if ret != 0:
+            raise StorageException('Failed to mount {} at {}: [ERR-{}] {}'.format(
+                devnode, mountpoint, ret, output))
+
+    def unmount_host(self, devnode):
+        # Check if actually mounted, and unmount until not mounted
+        (output, ret) = self._host_cmd('mount')
+        tries = 0
+        max_tries = 10
+
+        while re.search('{}.*'.format(devnode), output) and tries < max_tries:
+            (output, ret) = self._host_cmd('umount {}'.format(devnode))
+            if ret != 0:
+                raise StorageException('Unmount failed [ERR-{}] {}'.format(ret, output))
+            tries += 1
+            (output, ret) = self._host_cmd('mount')
+
+        if tries >= max_tries:
+            raise StorageException('Unmount failed after {} attempts'.format(tries))
+
     # TODO: Impliment these
-    def mount_host(self):
-        print("--- Manual Intervention Required! ---")
-        input("Mount host storage at /tmp/boardmount, then press <ENTER>")
-
-    def unmount_host(self):
-        print("--- Manual Intervention Required! ---")
-        input("Unmount /tmp/boardmount, then press <ENTER>")
-
     def mount_board(self):
         print("--- Manual Intervention Required! ---")
-        input("Mount board storage at /tmp/boardmount, then press <ENTER>")
 
     def unmount_board(self):
         print("--- Manual Intervention Required! ---")
-        input("Unmount /tmp/boardmount, then press <ENTER>")
+
+    def _host_cmd(self, cmd):
+        cmd = cmd.split()
+        cp = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        return (cp.communicate()[0].decode('utf-8'), cp.returncode)
