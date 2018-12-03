a613c861ea8e4c58e3c124419b0b7546453afb77
Merlin Webster <mwebster@witekio.com>
2018-12-03 17:10:10 +0000

    run_host_cmd now optionally takes args & kwargs

--- farmutils/helpers.py
+++ farmutils/helpers.py
@@ -1,11 +1,15 @@
 import subprocess
 
 
-def run_host_cmd(command):
+def run_host_cmd(command, *args, **kwargs):
     ''' Wrapper for subprocess.Popen
     Runs the command @command in a subproccess
     returns a tuple of (<command output>, <returncode>)
     '''
+    kwargs_final={'stdout': subprocess.PIPE, 'stderr': subprocess.PIPE}
+    if kwargs:
+        kwargs_final.update(kwargs)
+
     child_proc = subprocess.Popen(
-        command.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        command.split(), *args, **kwargs_final)
     return (child_proc.communicate()[0].decode('utf-8'), child_proc.returncode)
