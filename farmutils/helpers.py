b1c8b180435c4fc66215b9c1d0108a98f671184a
Merlin Webster <mwebster@witekio.com>
2018-12-05 18:46:47 +0000

    run_host_cmd: Upgrade, now takes stdin as argument

--- farmutils/helpers.py
+++ farmutils/helpers.py
@@ -1,15 +1,22 @@
 import subprocess
 
 
-def run_host_cmd(command, *args, **kwargs):
+def run_host_cmd(command, stdin=None, *args, **kwargs):
     ''' Wrapper for subprocess.Popen
     Runs the command @command in a subproccess
     returns a tuple of (<command output>, <returncode>)
     '''
-    kwargs_final={'stdout': subprocess.PIPE, 'stderr': subprocess.PIPE}
+    kwargs_final={
+        'stdin': subprocess.PIPE,
+        'stdout': subprocess.PIPE,
+        'stderr': subprocess.STDOUT
+    }
+
     if kwargs:
         kwargs_final.update(kwargs)
 
     child_proc = subprocess.Popen(
         command.split(), *args, **kwargs_final)
-    return (child_proc.communicate()[0].decode('utf-8'), child_proc.returncode)
+    return (child_proc.communicate(
+        input=None if not stdin else bytes(stdin, 'utf-8')
+    )[0].decode('utf-8'), child_proc.returncode)
