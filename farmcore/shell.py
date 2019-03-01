b450bed63d8cd25f574f7508a4162041bb2a3767
Merlin Webster <mwebster@witekio.com>
2019-03-01 10:25:35 +0000

    Removed depreciated & unused interact & shell classes

--- farmcore/shell.py
+++
@@ -1,8 +0,0 @@
-from farmclass import Farmclass
-
-
-class Shell(Farmclass):
-    """ Base class, should be inherited by all Shell Classes """
-    def __init__(self, console=None):
-        """ A Shell uses a Console object to interact with a board """
-        self.console = console
