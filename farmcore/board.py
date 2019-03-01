500cd523f3fa2764a1a08e733639f140ee5d3994
Merlin Webster <mwebster@witekio.com>
2019-03-01 10:58:07 +0000

    Board: Removed unneded comments & unused functions

--- farmcore/board.py
+++ farmcore/board.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python3
-
-# import farm
-# import hwconfig
-
 from pexpect import TIMEOUT, EOF
 
 
@@ -10,13 +5,17 @@ from .farmclass import Farmclass
 from .serialconsole import SerialConsole
 from .console import ExceptionKeywordRecieved
 
+
 DEFAULT_LOGFILE = object()
+
 
 class BootValidationError(Exception):
     pass
 
+
 class LoginError(Exception):
     pass
+
 
 class Board(Farmclass):
     def __init__(self, name, power=None, hub=None, muxpi=None,
@@ -79,55 +78,3 @@ class Board(Farmclass):
             password_match=self.login_pass_match,
             success_match=self.prompt
         )
-
-
-def get_board(boards, name):
-    for b in boards:
-        if b.name == name:
-            return b
-
-    raise NoBoard("Can't find board called [{}]".format(name))
-
-
-def get_name(hub):
-    for b in boards:
-        if b.hub == hub:
-            return b.name
-
-    return None
-
-
-def get(name, *a, **kw):
-    b = get_board(name)
-    print("{}".format(b))
-    return farm.farm(b, *a, **kw)
-
-
-def show_info(board):
-    u = usb.USB(board.hub)
-
-    print("\n =============== Device [{} - {}] =============".format(
-        u.usb_device, get_name(u.usb_device)))
-
-    blockinfo = u.filter_downstream({
-        'subsystem': 'block',
-        'devtype': 'disk'
-    }, {
-        'size': 0
-    })
-    for info in blockinfo:
-        print("Block device: {}, {:4.2f}MB".format(
-            info['devnode'], (info['size']/(1024 * 1024))))
-
-    serialinfo = u.filter_downstream({
-        'subsystem': 'tty'
-    })
-    for info in serialinfo:
-        print(("Serial device: {}, {}".format(
-            info['devnode'], info['vendor'])))
-
-    up = usb.USB(u.get_parent())
-    pinfo = up.devinfo
-    if pinfo:
-        print("Parent is: {} , {}".format(
-            pinfo['sysname'], pinfo['devtype']))
