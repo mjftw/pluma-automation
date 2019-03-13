1221dce1a74621ad4ebf2d04df938562dc047d84
Merlin Webster <mwebster@witekio.com>
2019-03-13 23:39:07 +0000

    Modem: Call recording now works, although audio sounds rubbish

--- farmcore/modem.py
+++ farmcore/modem.py
@@ -15,14 +15,23 @@ class ModemSim868(Farmclass):
         ''' @port: Port of the serial console for sending AT commands '''
         self.console = SerialConsole(port, 115200)
 
+        self._recording_lock = False
+
+    def AT_send(self, *args, **kwargs):
+        if self._recording_lock:
+            self.error('Cannot send AT commands while recording in progress',
+                ModemError)
+
+        return self.console.send(*args, **kwargs)
+
     def ready(self):
         # Check modem status
-        __, matched = self.console.send('AT', match='OK')
+        __, matched = self.AT_send('AT', match='OK')
         if not matched:
             return False
 
         # Check sim ready
-        __, matched = self.console.send('AT+CPIN?', match='\+CPIN: READY')
+        __, matched = self.AT_send('AT+CPIN?', match='\+CPIN: READY')
         if not matched:
             return False
 
@@ -36,7 +45,10 @@ class ModemSim868(Farmclass):
             3: Incoming call
             4: Ongoing call
         '''
-        __, matched = self.console.send('AT+CPAS', match='CPAS: [0-9]+')
+        if not self.ready():
+            self.error('Modem not ready', ModemError)
+
+        __, matched = self.AT_send('AT+CPAS', match='CPAS: [0-9]+')
         if not matched:
             self.error('Incorrect response from modem', ModemError)
 
@@ -76,7 +88,7 @@ class ModemSim868(Farmclass):
             self.error('Modem not ready', ModemError)
 
         # Enable call notification and wait for incoming
-        recieved, matched = self.console.send(
+        recieved, matched = self.AT_send(
             cmd='AT+CLIP=1', match='\+CLIP: "\+{0,1}[0-9]+"', timeout=timeout)
         if not matched:
             return None
@@ -89,7 +101,7 @@ class ModemSim868(Farmclass):
         self.log('Answering incoming call from {}'.format(caller))
 
         # Answer call
-        recieved, matched = self.console.send('ATA', match='OK')
+        recieved, matched = self.AT_send('ATA', match='OK')
 
         if record:
             raise NotImplementedError
@@ -104,7 +116,7 @@ class ModemSim868(Farmclass):
             self.log('No call ongoing')
             return False
 
-        __, matched = self.console.send('ATH', match='OK')
+        __, matched = self.AT_send('ATH', match='OK')
         if not matched:
             self.error('Could not end call', ModemError)
 
@@ -127,12 +139,12 @@ class ModemSim868(Farmclass):
 
         self.log('Calling {}'.format(number))
 
-        recieved, matched = self.console.send('ATD{};'.format(number),
+        recieved, matched = self.AT_send('ATD{};'.format(number),
             match=['OK', 'NO DIALTONE', 'BUSY', 'NO CARRIER', 'NO ANSWER'],
             excepts='ERROR')
         if not matched:
             self.error('Unexpected response from modem: {}'.format(recieved),
-            ModemError)
+                ModemError)
 
         if matched == 'OK':
             if not self.ongoing_call():
@@ -150,11 +162,82 @@ class ModemSim868(Farmclass):
 
         return False
 
-    def call_recording_start(self):
-        raise NotImplementedError
+    def call_recording_ongoing(self):
+        '''
+        Returns True if call recording in progress
+        '''
+        return self._recording_lock
 
-    def call_recording_stop(self):
-        raise NotImplementedError
+    def call_recording_start(self):
+        '''
+        Start a call recording.
+        Starting a recording causes modem to stream audio data over UART.
+        Starting recording stops AT commands from being sent unil recording stops.
+        '''
+        if self.call_recording_ongoing():
+            self.log('Cannot start recording, recording in progress')
+            return False
+
+        # Start a recording, with data sent at a 100ms interval,
+        # and a no sent at the end of transmission.
+        # __, matched = self.AT_send('AT+CRECORD=1,5,2', #last 2 bytes are crc
+        __, matched = self.AT_send('AT+CRECORD=1,5,0',
+            match=['OK', 'CRECORD'], excepts='ERROR')
+        if not matched:
+            self.error('Failed to start recording', ModemError)
+
+        # Flush serial input buffer
+        self.console.flush(True)
+
+        # From this point, direcly interract with Seiral Object
+        self._recording_lock = True
+
+        self.log('Started recording. Do not try to send AT commands before stopping')
+
+        return True
+
+    def call_recording_stop(self, file):
+        '''
+        Stop an ongoing call recording.
+        Returns True if recording saved to @file.
+        Returns False if no recording is ongoing.
+        File format is AMR 4.75k
+        '''
+        if not self.call_recording_ongoing():
+            return False
+
+        serial = self.console._ser
+
+        # Write any data to serial to stop recording
+        serial.write(self.console.encode('0'))
+
+        data = serial.readall()
+        self._recording_lock = False
+
+        # Strip out AT commands from start and end of data
+        if data.startswith(self.console.encode('\r\n+CRECORD:')):
+            data = data[len('\r\n+CRECORD:'):]
+        if data.endswith(self.console.encode('\r\nOK\r\n')):
+            data = data[:-len('\r\nOK\r\n')]
+
+        packets = []
+
+        with open(file, 'wb') as f:
+            # Write AMR file header
+            f.write(b'#*AMR\n')
+            f.write(data)
+
+            # Split data by packet header 0x7E
+            # for p in [p for p in data.split(b'\x7E') if p]:
+            #     # Fix replaced bytes
+            #     p.replace(b'\x7D\x5E', b'\x7E')
+            #     p.replace(b'\x7D\x5D', b'\x7D')
+            #     crc = p[-2:]
+            #     p = p[:-2]
+            #     #TODO: Calculate CRC and check it
+            #     f.write(p)
+
+        return True
 
     def send_sms(self, number, message):
         '''
@@ -164,18 +247,39 @@ class ModemSim868(Farmclass):
             self.error('Modem not ready', ModemError)
 
         # Set SMS to text mode
-        __, matched = self.console.send('AT+CMGF=1', match='OK')
+        __, matched = self.AT_send('AT+CMGF=1', match='OK')
         if not matched:
             self.error('Failed to set SMS mode to text', ModemError)
 
         # Start writing text
-        __, matched = self.console.send('AT+CMGS="{}"'.format(number), match='>')
+        __, matched = self.AT_send('AT+CMGS="{}"'.format(number), match='>')
         if not matched:
             self.error('SMS start command failed', ModemError)
-        __, matched = self.console.send('{}\x1A'.format(message),
+        __, matched = self.AT_send('{}\x1A'.format(message),
             match='CMGS', timeout=20)
         if not matched:
             self.error('Failed to send SMS', ModemError)
 
         self.log('SMS sent')
 
+    def read_sms(self, sms_number=None):
+        '''
+        Returns a list of all SMS messages recieved if @sms_number
+        is None, else returns only SMS at @sms_number.
+        Returns None if SMS at @sms_number does not exist.
+        '''
+        raise NotImplementedError
+
+    def delete_sms(self, sms_number=None):
+        '''
+        Deletes the sms at @sms_number if it exists.
+        If @sms_number is None, deletes all SMS.
+        Returns True if delete successfull, or False if not
+        '''
+        raise NotImplementedError
+
+    def sms_recieved(self):
+        '''
+        Returns the number of SMS recieved
+        '''
+        raise NotImplementedError
