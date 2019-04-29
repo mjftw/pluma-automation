56f8d45b4658f220d59af3112c4d57df0a85c1f3
Merlin Webster <mwebster@witekio.com>
2019-04-29 11:17:26 +0100

    Added generic StockBootTest to stock

--- farmtest/stock/__init__.py
+++ farmtest/stock/__init__.py
@@ -1,3 +1,4 @@
 from .deffuncs import TestMustBeInController, sc_run_n_iterations,\
     sc_time_in_range, sc_run_daily_at_hour, tt_log_stats, ss_log_test_plan,\
     sr_log_test_results, sleep_and_notify, console_is_alive
+from .tests import StockBootTest
