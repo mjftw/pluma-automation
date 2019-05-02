87137cd7f95e211f61f27f3e214d7973eac3f44a
Merlin Webster <mwebster@witekio.com>
2019-05-02 12:48:48 +0100

    StorageBase: mount() & unmount() more robust

--- farmcore/storagebase.py
+++ farmcore/storagebase.py
@@ -1,5 +1,6 @@
 import re
 import os
+import time
 import tempfile
 
 from farmutils.helpers import run_host_cmd
@@ -25,43 +26,73 @@ class StorageBase():
         ''' Switch storage to the board '''
         raise NotImplemented('This method must be implimented by inheriting class')
 
-    def mount_host(self, devnode, mountpoint=None):
+    def mount_host(self, devnode, mountpoint=None, max_tries=5):
         mountpoint = mountpoint or self.host_mountpoint
         if not mountpoint:
             self.host_mountpoint = tempfile.mkdtemp(suffix='_lab')
             mountpoint = self.host_mountpoint
 
-        # Check if already mounted
-        (output, ret) = run_host_cmd('mount')
-
-        # Unmount if mounted
-        if re.search('{}.*'.format(devnode), output):
-            self.unmount_host(devnode)
-
-        if not os.path.isdir(mountpoint):
-            os.mkdir(mountpoint)
-
-        (output, ret) = run_host_cmd('mount {} {}'.format(devnode, mountpoint))
-        if ret != 0:
-            raise StorageException('Failed to mount {} at {}: [ERR-{}] {}'.format(
-                devnode, mountpoint, ret, output))
-
-    def unmount_host(self, devnode):
-        # Check if actually mounted, and unmount until not mounted
-        (output, ret) = run_host_cmd('mount')
-        tries = 0
-        max_tries = 10
-
-        while re.search('{}.*'.format(devnode), output) and tries < max_tries:
-            (output, ret) = run_host_cmd('umount {}'.format(devnode))
-            if ret != 0:
-                raise StorageException('Unmount failed [ERR-{}] {}'.format(ret, output))
-            tries += 1
+        success = False
+        for _ in range(0, max_tries):
+            # Check if already mounted
             (output, ret) = run_host_cmd('mount')
 
-        if tries >= max_tries:
-            raise StorageException('Unmount failed after {} attempts'.format(tries))
+            # Unmount if mounted
+            if re.search('{}.*'.format(devnode), output):
+                self.unmount_host(devnode)
 
+            if not os.path.isdir(mountpoint):
+                os.mkdir(mountpoint)
+
+            (output, ret) = run_host_cmd('mount {} {}'.format(devnode, mountpoint))
+            if ret == 0:
+                success = True
+                break
+
+            time.sleep(1)
+
+        if not success:
+            raise StorageException('Failed to mount {} at {} after {} attemps: [ERR-{}] {}'.format(
+                devnode, mountpoint, max_tries, ret, output))
+
+    def unmount_host(self, devnode, max_tries=5):
+        # Check if actually mounted, and unmount until not mounted
+
+        errno = None
+        errmsg = None
+        max_umounts_per_try = 10
+
+        success = False
+        for _ in range(0, max_tries):
+            if success:
+                break
+            for _ in range(0, max_umounts_per_try):
+                if self.devnode_is_mounted(devnode):
+                    (output, ret) = run_host_cmd('umount {}'.format(devnode))
+                    if ret == 0:
+                        if not self.devnode_is_mounted(devnode):
+                            success = True
+                            break
+                    else:
+                        errno = ret
+                        errmsg = output
+                else:
+                    success = True
+                    break
+
+            time.sleep(1)
+
+        if not success:
+            raise StorageException(
+                'Unmount failed after {} attempts[ERR-{}] {}'.format(
+                    max_tries, errno, errmsg))
+
+    def devnode_is_mounted(self, devnode):
+        (output, ret) = run_host_cmd('mount')
+        if re.search('{}.*'.format(devnode), output):
+            return True
+        else:
+            return False
     # TODO: Impliment these
     def mount_board(self):
         print("--- Manual Intervention Required! ---")
