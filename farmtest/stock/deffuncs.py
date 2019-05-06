19cdc6a4531c6bb721a74dbcbf45f99a040ed245
Merlin Webster <mwebster@witekio.com>
2019-05-06 11:54:59 +0100

    Deffuncs: added write_global_data() report func

--- farmtest/stock/deffuncs.py
+++ farmtest/stock/deffuncs.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import json
 import time
 
 from ..unittest import deferred_function
@@ -7,6 +8,99 @@ from ..unittest import deferred_function
 class TestMustBeInController(Exception):
     pass
 
+
+# ==== UnitTest Teardown functions ====
+
+@deferred_function
+def tt_log_stats(TestController, board):
+    if TestController is None:
+        raise TestMustBeInSController
+    board.log("Test #: {}, pass #: {}, fail #: {}".format(
+        TestController.stats['num_tests_run'],
+        TestController.stats['num_tests_pass'],
+        TestController.stats['num_tests_fail']
+        ))
+
+
+# ==== TestController Setup functions ====
+
+@deferred_function
+def ss_log_test_plan(TestController, board):
+    message = "Starting test TestController:\n"
+    if TestController.setup:
+        message += "\t{}: {}\n".format(
+            TestController.setup['name'],
+            TestController.setup['str'])
+    if TestController.run_condition:
+        message += "\t{}: {}\n".format(
+            TestController.run_condition['name'],
+            TestController.run_condition['str'])
+    if TestController.report:
+        message += "\t{}: {}\n".format(
+            TestController.report['name'],
+            TestController.report['str'])
+    message += str(TestController.tests)
+    board.log(message)
+
+
+# ==== TestController Report functions ====
+
+@deferred_function
+def sr_log_test_results(TestController, board):
+    print("In func {}".format(__name__))
+    message = "Test TestController results: \n".format(TestController.name)
+    for test in TestController.tests_passed:
+        message += "\t{}: PASS\n".format(
+            test.body['str'])
+    for test in TestController.tests_failed:
+        message += "\t{}: FAIL\n".format(
+            test.body['str'])
+    message += "Total tests:\n"
+    message += "\tRun = {}\n".format(TestController.stats['num_tests_run'])
+    message += "\tPass = {}\n".format(TestController.stats['num_tests_pass'])
+    message += "\tFail = {}\n".format(TestController.stats['num_tests_fail'])
+    message += "Total iterations:\n"
+    message += "\tRun = {}\n".format(TestController.stats['num_iterations_run'])
+    message += "\tPass = {}\n".format(TestController.stats['num_iterations_pass'])
+    message += "\tFail = {}\n".format(TestController.stats['num_iterations_fail'])
+    board.log(message)
+
+
+@deferred_function
+def write_global_data(TestController, output_file, log_func=print):
+    log_func('Writing testing summary data to {}...'.format(
+        output_file))
+    with open(output_file, 'w') as f:
+        json_data = json.dumps(TestController.data, indent=4)
+        f.write(json_data)
+
+
+# ====== TestController Run Condition Functions ======
+
+@deferred_function
+def sleep_and_notify(duration, unit, mode, log_func=print):
+    log_func("Now waiting {} {}...".format(
+        duration, unit))
+    for duration in reversed(range(1, duration+1)):
+        log_func("{} {} to go...".format(duration, unit))
+        if unit == 'hours':
+            time.sleep(60*60)
+        if unit == 'minutes':
+            time.sleep(60)
+        if unit == 'seconds':
+            time.sleep(1)
+
+
+@deferred_function
+def console_is_alive(console):
+    try:
+        return console.check_alive()
+    except KeyboardInterrupt as e:
+        raise e
+    except InterruptedError as e:
+        raise e
+    except Exception as e:
+        return False
 
 @deferred_function
 def sc_run_n_iterations(TestController, ntimes):
@@ -50,80 +144,4 @@ def sc_run_in_datetime_range(datetime_start, datetime_end):
     if datetime_end >= datetime.now() >= datetime_start:
         return True
     else:
-        return False
-
-@deferred_function
-def tt_log_stats(TestController, board):
-    if TestController is None:
-        raise TestMustBeInSController
-    board.log("Test #: {}, pass #: {}, fail #: {}".format(
-        TestController.stats['num_tests_run'],
-        TestController.stats['num_tests_pass'],
-        TestController.stats['num_tests_fail']
-        ))
-
-
-@deferred_function
-def ss_log_test_plan(TestController, board):
-    message = "Starting test TestController:\n"
-    if TestController.setup:
-        message += "\t{}: {}\n".format(
-            TestController.setup['name'],
-            TestController.setup['str'])
-    if TestController.run_condition:
-        message += "\t{}: {}\n".format(
-            TestController.run_condition['name'],
-            TestController.run_condition['str'])
-    if TestController.report:
-        message += "\t{}: {}\n".format(
-            TestController.report['name'],
-            TestController.report['str'])
-    message += str(TestController.tests)
-    board.log(message)
-
-
-@deferred_function
-def sr_log_test_results(TestController, board):
-    print("In func {}".format(__name__))
-    message = "Test TestController results: \n".format(TestController.name)
-    for test in TestController.tests_passed:
-        message += "\t{}: PASS\n".format(
-            test.body['str'])
-    for test in TestController.tests_failed:
-        message += "\t{}: FAIL\n".format(
-            test.body['str'])
-    message += "Total tests:\n"
-    message += "\tRun = {}\n".format(TestController.stats['num_tests_run'])
-    message += "\tPass = {}\n".format(TestController.stats['num_tests_pass'])
-    message += "\tFail = {}\n".format(TestController.stats['num_tests_fail'])
-    message += "Total iterations:\n"
-    message += "\tRun = {}\n".format(TestController.stats['num_iterations_run'])
-    message += "\tPass = {}\n".format(TestController.stats['num_iterations_pass'])
-    message += "\tFail = {}\n".format(TestController.stats['num_iterations_fail'])
-    board.log(message)
-
-
-@deferred_function
-def sleep_and_notify(duration, unit, mode, log_func=print):
-    log_func("Now waiting {} {}...".format(
-        duration, unit))
-    for duration in reversed(range(1, duration+1)):
-        log_func("{} {} to go...".format(duration, unit))
-        if unit == 'hours':
-            time.sleep(60*60)
-        if unit == 'minutes':
-            time.sleep(60)
-        if unit == 'seconds':
-            time.sleep(1)
-
-
-@deferred_function
-def console_is_alive(console):
-    try:
-        return console.check_alive()
-    except KeyboardInterrupt as e:
-        raise e
-    except InterruptedError as e:
-        raise e
-    except Exception as e:
         return False
