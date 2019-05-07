229b96edc658527efac4ff72516acf60d71e7b70
Merlin Webster <mwebster@witekio.com>
2019-05-07 12:35:15 +0100

    farmtest.stock: fixed import in init

--- farmtest/stock/__init__.py
+++ farmtest/stock/__init__.py
@@ -1,4 +1,3 @@
-from .exceptions import *
 from .deffuncs import sc_run_n_iterations,\
     sc_time_in_range, sc_run_daily_at_hour, tt_log_stats, ss_log_test_plan,\
     sr_log_test_results, sleep_and_notify, sc_run_in_datetime_range,\
