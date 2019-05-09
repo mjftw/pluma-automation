fdc3cb199ec13c9a8f80950295ded76fa6f47753
Merlin Webster <mwebster@witekio.com>
2019-05-09 10:29:19 +0100

    Graphing: Added generic boot test success rate graphing: 	- Using panadas and pygal, we can now graph how the frequency 	  of successful boots changes over multiple iterations

--- farmutils/__init__.py
+++ farmutils/__init__.py
@@ -5,3 +5,4 @@ from .git import reset_repos, get_latest_tag, get_tag_list, \
     version_is_valid, filter_versions, compile_version_list
 from .helpers import run_host_cmd, format_json_tinydb
 from .sampling import AsyncSampler
+from .graphing import boot_graph
