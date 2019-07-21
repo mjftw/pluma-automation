404d3a2e66cb57965b0c01ca8214d9695bc6eb3d
Phil Robinson <probinson@witekio.com>
2019-07-21 16:16:31 +0000

    setup.py created online with Bitbucket

---
+++ setup.py
@@ -0,0 +1,13 @@
+import sys
+from setuptools import setup
+setup(
+    name = "farm-core",
+    version = "0.1",
+    packages=["farm-core"],
+    author="Merlin Webster",
+    author_email = "mwebster@witekio.com",
+    description = "farm-core package for the Automation Lab.",
+    license = "MIT",
+    keywords= "automation, lab, testing",
+    url = "",
+)
