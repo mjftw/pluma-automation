2d4ecd4f1edd4f6aef79c94056ffadcb078b5c0f
Merlin Webster <mwebster@witekio.com>
2019-07-31 14:05:57 +0100

    Helpers: Added getch() and seech() functions

--- farmutils/__init__.py
+++ farmutils/__init__.py
@@ -3,6 +3,7 @@ from .email import Email, send_exception_email
 from .git import reset_repos, get_latest_tag, get_tag_list, \
     version_is_valid, filter_versions, compile_version_list
 from .helpers import run_host_cmd, format_json_tinydb, \
-    timestamp_to_datetime, datetime_to_timestamp, regex_filter_list
+    timestamp_to_datetime, datetime_to_timestamp, regex_filter_list, \
+    getch, seech
 from .sampling import AsyncSampler
 from .graphing import boot_graph
--- farmutils/helpers.py
+++ farmutils/helpers.py
@@ -65,3 +65,39 @@ def regex_filter_list(patterns, items, unique=None):
     if unique:
         gen = set(gen)
     return sorted(gen)
+
+
+def getch():
+    '''
+    Get a single character from stdin and return it.
+    Function blocks until character recieved.
+    '''
+    from sys import stdin
+    from tty import setraw
+    from termios import tcgetattr, tcsetattr, TCSADRAIN
+
+    fd = stdin.fileno()
+    old_settings = tcgetattr(fd)
+    setraw(stdin.fileno())
+
+    ch = stdin.read(1)
+
+    tcsetattr(fd, TCSADRAIN, old_settings)
+
+    return ch
+
+
+def seech(encoding='utf-8'):
+    '''
+    Print key presses with corresponding hex value.
+    Function blocks until Ctrl-C is pressed.
+    '''
+    from curses.ascii import unctrl
+
+    key = None
+    keyhex = None
+    print('Echoing key values. Press Ctrl-C to exit')
+    while keyhex != b'\x03':
+        key = getch()
+        keyhex = key.encode(encoding)
+        print(f'Pressed: {unctrl(key)} Hex: {keyhex}')
