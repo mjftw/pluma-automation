8ac602a244f68e65132bd761ee53ff7495442f24
Merlin Webster <mwebster@witekio.com>
2019-09-19 17:43:35 +0100

    Async method: switched to class structure

--- async-ideas.py
+++ async-ideas.py
@@ -1,13 +1,37 @@
-import threading
+from multiprocessing.pool import ThreadPool
+from threading import Thread
 import time
 
 
-def nonblocking(f):
-    def wrap(*args, **kwargs):
-        thread = threading.Thread(target=f, args=args, kwargs=kwargs)
-        thread.start()
+class nonblocking:
+    def __init__(self, fn):
+        self.fn = fn
+        self.pool = ThreadPool(processes=1)
+        self.async_result = None
 
-    return wrap
+    @property
+    def retval(self):
+        if self.async_result:
+            return self.async_result.get()
+
+    def __get__(self, instance, owner):
+        if instance is None:
+            return self
+
+        d = self
+        # use a lambda to produce a bound method
+        mfactory = lambda self, *args, **kw: d(self, *args, **kw)
+        mfactory.__name__ = self.fn.__name__
+        mfactory.retval = self.retval
+
+        return mfactory.__get__(instance, owner)
+
+    def __call__(self, *args, **kwargs):
+        def wrap(*args, **kwargs):
+            self.async_result = self.pool.apply_async(
+                func=self.fn, args=args, kwds=kwargs)
+
+        return wrap(*args, **kwargs)
 
 
 class Foo:
@@ -16,15 +40,15 @@ class Foo:
         self.last_data = None
 
     @nonblocking
-    def slow_method(self):
-        print("slow menthod started")
+    def slow_method(self, arg1):
+        print("slow method started")
 
         for i in range(0, 10):
             self.data.append(i)
             self.save_last_data(i)
             time.sleep(1)
 
-        print("slow menthod finished")
+        print("slow method finished")
 
         # How to get return value out?
         return 'Hello world!'
@@ -32,3 +56,4 @@ class Foo:
     def save_last_data(self, i):
         self.last_data = self.data[-1]
 
+f = Foo()
