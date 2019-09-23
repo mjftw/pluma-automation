d49f310d6bfafbc6b8089449fc4fdcaed572df2c
Merlin Webster <mwebster@witekio.com>
2019-09-23 13:01:21 +0100

    Async: Nonblocking moved to same file as AsyncSampler

--- farmutils/asynchronous.py
+++ farmutils/asynchronous.py
@@ -1,14 +1,14 @@
+from multiprocessing.pool import ThreadPool
 import threading
 import time
-
-# Known issues:
-#   When exiting the main thread, while the sampler thread is running,
-#   the sampler thread does not terminate
 
 
 class AsyncSampler():
     '''Asynchronusly run a sample function in a separate thread at a given
         frequecy, to get a list of tuples of samples and times'''
+    # Known issues:
+    #   When exiting the main thread, while the sampler thread is running,
+    #   the sampler thread does not terminate
     def __init__(self, sample_func):
         if not callable(sample_func):
             raise AttributeError('sample_func must be callable')
@@ -61,3 +61,56 @@ class AsyncSampler():
             sleep_time = 1.0/frequency - (time.time()-start_time)
             if sleep_time > 0:
                 time.sleep(sleep_time)
+
+
+class Nonblocking:
+    def __init__(self):
+        self._thread_pool = ThreadPool(processes=1)
+        self._running_async = False
+
+    class method:
+        def __init__(self, fn):
+            self.fn = fn
+            self.async_result = None
+
+            self.parent = None
+
+        def get_return(self):
+            if self.async_result:
+                return self.async_result.get()
+
+        def __get__(self, instance, owner):
+            if instance is None:
+                return self
+
+            d = self
+            # use a lambda to produce a bound method
+            mfactory = lambda self, *args, **kw: d(self, *args, **kw)
+            mfactory.__name__ = self.fn.__name__
+            mfactory.get_return = self.get_return
+
+            if not self.parent:
+                # Make link to parent class
+                self.parent = instance
+
+            return mfactory.__get__(instance, owner)
+
+        def __call__(self, *args, **kwargs):
+            if self.parent._running_async:
+                return self.fn(*args, **kwargs)
+
+            def wrap_outer(fn):
+                def wrap_inner(*args, **kwargs):
+                    self.parent._running_async = True
+                    retval = fn(*args, **kwargs)
+                    self.parent._running_async = False
+
+                    return retval
+                return wrap_inner
+
+            fn = wrap_outer(self.fn)
+
+            self.async_result = self.parent._thread_pool.apply_async(
+                func=fn, args=args, kwds=kwargs)
+
+            return self
