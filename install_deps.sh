4146c591815eb4ae581fd4316826f27c5a3a42b3
Merlin Webster <mwebster@witekio.com>
2019-09-23 17:55:34 +0100

    install_deps: script now only uses sudo if it needs to

--- install_deps.sh
+++ install_deps.sh
@@ -3,8 +3,14 @@
 # Dependency installer script for Debian based Linux distributions
 #
 
+# Install as sudo if sudo command installed, and user is not root
+SUDO=""
+if [ ! -z "$(which sudo)" -a "$UID" != "0" ]; then
+    SUDO="sudo"
+fi
+
 # LibUSB required for PyFTDI library used by SDWire
-sudo apt install -y libusb-1.0 python3 python3-pip graphviz
+$SUDO apt install -y libusb-1.0 python3 python3-pip graphviz
 
 # Install Python libraries
 pip3 install -r requirements.txt
