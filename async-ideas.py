70ac342fba068278fe6357ca59fa86905b1873c4
Merlin Webster <mwebster@witekio.com>
2019-09-23 12:47:47 +0100

    Async: async functions can now call others without making more threads

--- async-ideas.py
+++ async-ideas.py
@@ -5,6 +5,7 @@ import time
 class nonblocking:
     def __init__(self):
         self._thread_pool = ThreadPool(processes=1)
+        self._running_async = False
 
     class method:
         def __init__(self, fn):
@@ -28,17 +29,35 @@ class nonblocking:
             mfactory.__name__ = self.fn.__name__
             mfactory.retval = self.retval
 
-            # Make link to parent class
-            self.parent = instance
+            if not self.parent:
+                # Make link to parent class
+                self.parent = instance
 
             return mfactory.__get__(instance, owner)
 
         def __call__(self, *args, **kwargs):
-            def wrap(*args, **kwargs):
-                self.async_result = self.parent._thread_pool.apply_async(
-                    func=self.fn, args=args, kwds=kwargs)
+            if self.parent._running_async:
+                return self.fn(*args, **kwargs)
 
-            return wrap(*args, **kwargs)
+            def wrap_outer(fn):
+                def wrap_inner(*args, **kwargs):
+                    print('=== Entering worker thread ===')
+                    self.parent._running_async = True
+
+                    retval = fn(*args, **kwargs)
+
+                    self.parent._running_async = False
+                    print('=== Exiting worker thread ===')
+
+                    return retval
+                return wrap_inner
+
+            fn = wrap_outer(self.fn)
+
+            self.async_result = self.parent._thread_pool.apply_async(
+                func=fn, args=args, kwds=kwargs)
+
+
 
 
 class Foo(nonblocking):
@@ -59,7 +78,19 @@ class Foo(nonblocking):
 
         print("slow method finished")
 
-        return 'Hello world!'
+        return 'Hello'
+
+    @nonblocking.method
+    def stacking_slow_method(self, *args, **kwargs):
+        print("stacking_slow_method started")
+        print(args, kwargs)
+
+        time.sleep(1)
+        retval = self.slow_method(None)
+
+        print("stacking_slow_method finished")
+
+        return f'{retval} World!'
 
     def save_last_data(self, i):
         self.last_data = self.data[-1]
