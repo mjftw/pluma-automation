bbd7f9d9082ac9410fbae9d62895239746df81b0
Merlin Webster <mwebster@witekio.com>
2019-10-21 15:23:58 +0100

    Packaging: Added --dev option comments to install_deps.sh

--- install_deps.sh
+++ install_deps.sh
@@ -17,10 +17,20 @@ pip3 install --user -r requirements.txt
 
 # Install farm-core packages (farmcore, farmtest, farmutils)
 if [ "$1" == "-d" -o "$1" == "--dev" ]; then
+    # Install packages as a farm-core package developer.
+    # The current dir is used as the package root, and any edits made to python
+    #   scripts here WILL be picked up by installed package.
+    # This option is to be used when developing the farm-core packages.
+
     echo "=== Installing farm-core packages (farmcore, farmtest, farmutils), editable from $PWD (dev mode) ==="
     pip3 uninstall -y farm-core
     pip3 install --user --editable .
 else
+    # Install farm-core packages as farm-core package user.
+    # Edits in local farm-core dir will not be used by installed packages, and
+    #   package must be reinstalled using script to track these changes.
+    # This is what we would want for a normal user.
+
     echo "=== Installing farm-core packages (farmcore, farmtest, farmutils) ==="
     pip3 uninstall -y farm-core
     pip3 install --user .
