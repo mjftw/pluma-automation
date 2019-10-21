c48050e47fb8cddb57f9ef729a64b3816848ea7e
Merlin Webster <mwebster@witekio.com>
2019-10-21 15:30:42 +0100

    install_deps.sh: Changes allow calling from any dir

--- install_deps.sh
+++ install_deps.sh
@@ -9,11 +9,13 @@ if [ ! -z "$(which sudo)" -a "$UID" != "0" ]; then
     SUDO="sudo"
 fi
 
+PROJECT_ROOT="$(dirname "$0")"
+
 # LibUSB required for PyFTDI library used by SDWire
 $SUDO apt install -y libusb-1.0 python3 python3-pip graphviz
 
 # Install Python libraries
-pip3 install --user -r requirements.txt
+pip3 install --user -r $PROJECT_ROOT/requirements.txt
 
 # Install farm-core packages (farmcore, farmtest, farmutils)
 if [ "$1" == "-d" -o "$1" == "--dev" ]; then
@@ -22,9 +24,9 @@ if [ "$1" == "-d" -o "$1" == "--dev" ]; then
     #   scripts here WILL be picked up by installed package.
     # This option is to be used when developing the farm-core packages.
 
-    echo "=== Installing farm-core packages (farmcore, farmtest, farmutils), editable from $PWD (dev mode) ==="
+    echo "=== Installing farm-core packages (farmcore, farmtest, farmutils), editable from $PROJECT_ROOT (dev mode) ==="
     pip3 uninstall -y farm-core
-    pip3 install --user --editable .
+    pip3 install --user --editable $PROJECT_ROOT
 else
     # Install farm-core packages as farm-core package user.
     # Edits in local farm-core dir will not be used by installed packages, and
@@ -33,5 +35,5 @@ else
 
     echo "=== Installing farm-core packages (farmcore, farmtest, farmutils) ==="
     pip3 uninstall -y farm-core
-    pip3 install --user .
+    pip3 install --user $PROJECT_ROOT
 fi
