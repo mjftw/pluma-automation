53fd5f8bf30ac14f38e3ac17609ca42ad91f338e
Angelo_Witekio <asison@witekio.com>
2019-12-17 03:20:26 -0800

    add abc decorator for powerbase

--- farmcore/baseclasses/powerbase.py
+++ farmcore/baseclasses/powerbase.py
@@ -6,12 +6,14 @@ class PowerBase(Farmclass, metaclass=ABCMeta):
     def __init__(self, reboot_delay=None):
         self.reboot_delay = reboot_delay or 0.5
 
+    @abstractmethod
     def on(f):
         def wrap(self, *args, **kwargs):
             self.log(f'{str(self)}: Power on')
             f(self, *args, **kwargs)
         return wrap
 
+    @abstractmethod
     def off(f):
         def wrap(self, *args, **kwargs):
             self.log(f'{str(self)}: Power off')
