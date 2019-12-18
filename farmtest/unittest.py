c31a744236ffe69034cd947c4129486ff5bb252a
Angelo_Witekio <asison@witekio.com>
2019-12-18 05:40:19 -0800

    Remove unitTest class

--- farmtest/unittest.py
+++ farmtest/unittest.py
@@ -1,11 +1,6 @@
 import inspect
 from copy import copy
 
-
-class TestFunctionNotSet(Exception):
-    pass
-
-
 class deferred_function():
     """ Can be used as a function decorator """
     def __init__(self, f, *args, **kwargs):
@@ -55,60 +50,3 @@ class deferred_function():
             return self.f(*args, **self.kwargs)
         else:
             return self.f(*args)
-
-
-class UnitTest():
-    def __init__(self, fbody=None, fsetup=None, fteardown=None, log_func=None):
-        self.success = False
-
-        self.body = fbody
-        self.setup = fsetup
-        self.teardown = fteardown
-
-    def __repr__(self):
-        funcs = "{}:".format(__class__.__name__)
-        funcs += "\n\tSetup: {}".format(str(self.setup))
-        funcs += "\n\tBody: {}".format(str(self.body))
-        funcs += "\n\tTeardown: {}".format(str(self.teardown))
-        return funcs
-
-    @property
-    def body(self):
-        return self._body
-
-    @body.setter
-    def body(self, f):
-        self._body = None if f is None else deferred_function(f)
-
-    @property
-    def setup(self):
-        return self._setup
-
-    @setup.setter
-    def setup(self, f):
-        self._setup = None if f is None else deferred_function(f)
-
-    @property
-    def teardown(self):
-        return self._teardown
-
-    @teardown.setter
-    def teardown(self, f):
-        self._teardown = None if f is None else deferred_function(f)
-
-    def run(self, controller=None):
-        if self.setup:
-            self.setup.run(controller)
-
-        if not self.body:
-            raise TestFunctionNotSet
-
-        self.success = self.body.run(controller)
-
-        if self.setup and self.teardown:
-            self.teardown.run(controller)
-
-        if self.success:
-            return True
-        else:
-            return False
