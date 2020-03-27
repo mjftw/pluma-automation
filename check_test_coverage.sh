e092b786d047a70a6ba58f85809c5a90f3f6c0e2
Merlin Webster <mwebster@witekio.com>
2020-03-27 14:47:30 +0000

    Fixed check_test_coverage.sh - Was checking coverage of farm-example

--- check_test_coverage.sh
+++ check_test_coverage.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-thisdir="$(dirname $0)"
-pytest $thisdir --cov=$thisdir --cov-report=term-missing $@
+this_dir="$(dirname $0)"
+farmcore_dir="$(pip3 show farm-core | awk -F': ' '/Location/ {print $2}')"
+
+if [ -z "${farmcore_dir}" ]; then
+    echo 'Cannot find pip3 package "farm-core". Is it installed?'
+    exit 1
+fi
+
+pytest --cov=$farmcore_dir --cov-report=term-missing $this_dir $@
