a7ed376f8d92d2a9c770ddb080923df0f15da82a
Merlin Webster <mwebster@witekio.com>
2020-04-22 21:00:46 +0100

    Added setup info for USB Relay tests

--- rpi3/test_USBRelay.py
+++ rpi3/test_USBRelay.py
@@ -1,11 +1,55 @@
+''' farmcore.USBRelay class tests
+
+These tests require that the USB Relay (DLP-IOR4) be set up in a certain way.
+Four GPIOs on the RPi must also be connected to the USB Relay.
+The RPi pins used for this should be set in the config file "hardware.json".
+
+The USB path for the USB Relay must also be in "hardware.json".
+This can be found in Linux by running "dmesg -w", then plugging in the USB
+relay.
+
+The output should look like:
+[ 7570.491865] usb 1-1.2: Product: DLP-IOR4
+[ 7570.491877] usb 1-1.2: Manufacturer: DLP Design
+
+In this case the USB path is "1-1.2".
+This path will change depending on what USB port/ USB hub port the USB device
+is connected to.
+
+The USB Relay should be set up with wires connecting the terminal blocks
+and RPi GPIOs as shown below:
+
+### Connections for A ports ###
+RPi_GPIO -> 1_A
+1_common -> 2_A
+2_common -> 3_A
+3_common -> 4_A
+4_common -> RPi_GPIO
+
+When all switches are set to port A, a connection is made between the
+output GPIO on 1_A and the input GPIO on 4_common
+
+### Connections for B ports ###
+RPi_GPIO -> 1_common
+1_B      -> 2_common
+2_B      -> 3_common
+3_B      -> 4_common
+4_B      -> RPi_GPIO
+
+When all switches are set to port A, a connection is made between the
+output GPIO on 1_common and the input GPIO on 4_B.
+'''
+
 import RPi.GPIO as GPIO
 import time
 
 from fixtures import relay_pins, usb_relay
 
 def assert_continuity(output_pin, input_pin, repititions=100):
-    # Toggle output and check input matches
-    # Repeat many times to rule out floating input matching output by chance
+    ''' Toggle output and check input matches to assert continuity
+    Repeat many times to rule out floating input matching output by chance
+    '''
+
     for __ in range(0, repititions):
         GPIO.output(output_pin, GPIO.HIGH)
         time.sleep(0.001)
