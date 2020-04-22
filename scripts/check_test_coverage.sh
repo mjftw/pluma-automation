3c39048281a718b1575f41367e1de04bfaa82f4b
Merlin Webster <mwebster@witekio.com>
2020-04-22 11:51:00 +0100

    Added back in script to check test coverage

--- scripts/check_test_coverage.sh
+++ scripts/check_test_coverage.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 this_dir="$(dirname $0)"
-
 test_dir="$(readlink -f $this_dir/..)"
+farmcore_dir="$(pip3 show farm-core | awk -F': ' '/Location/ {print $2}')"
+
+if [ -z "${farmcore_dir}" ]; then
+    echo 'Cannot find pip3 package "farm-core". Is it installed?'
+    exit 1
+fi
+
 if [ ! -z "$1" ]; then
     test_dir="$(readlink -f $this_dir/../$1)"
 
@@ -15,4 +21,4 @@ else
     echo "Running all tests in $test_dir"
 fi
 
-pytest "$test_dir" -v
+pytest --cov=$farmcore_dir --cov-report=term-missing $test_dir $@
