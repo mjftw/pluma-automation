a9a30da46fe1308054e8c544e78d31203c9ddb30
Merlin Webster <mwebster@witekio.com>
2020-04-23 20:22:52 +0100

    Refactor fixtures to reduce code duplication

--- rpi/fixtures.py
+++ rpi/fixtures.py
@@ -10,6 +10,22 @@ def usb_path_regex():
     return re.compile(r'([-.]{0,1}[1-9]+)+')
 
 
+def read_config_usb_path(name):
+    assert isinstance(name, str)
+
+    conf = read_config()
+
+    check_schema(conf,
+        {
+            'usb_paths': {
+                name: usb_path_regex()
+            }
+        }
+    )
+
+    return conf['usb_paths'][name]
+
+
 @fixture
 def relay_pins():
     conf = read_config()
@@ -36,59 +52,19 @@ def relay_pins():
 
 @fixture
 def usb_relay():
-    conf = read_config()
-
-    check_schema(conf,
-        {
-            "usb_paths": {
-                "usb_relay": usb_path_regex()
-            }
-        }
-    )
-
-    return USBRelay(conf['usb_paths']['usb_relay'])
+    return USBRelay(read_config_usb_path('usb_relay'))
 
 
 @fixture
 def hub():
-    conf = read_config()
-
-    check_schema(conf,
-        {
-            "usb_paths": {
-                "hub": usb_path_regex()
-            }
-        }
-    )
-
-    return Hub(conf['usb_paths']['hub'])
+    return Hub(read_config_usb_path('hub'))
 
 
 @fixture
 def hub_usb_serial_path():
-    conf = read_config()
-
-    check_schema(conf,
-        {
-            "usb_paths": {
-                "hub_usb_serial": usb_path_regex()
-            }
-        }
-    )
-
-    return conf['usb_paths']['hub_usb_serial']
+    return read_config_usb_path('hub_usb_serial')
 
 
 @fixture
 def hub_usb_relay_path():
-    conf = read_config()
-
-    check_schema(conf,
-        {
-            "usb_paths": {
-                "hub_usb_relay": usb_path_regex()
-            }
-        }
-    )
-
-    return conf['usb_paths']['hub_usb_relay']
+    return read_config_usb_path('hub_usb_relay')
