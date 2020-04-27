e8920e8648264b0f49c00fb3a0440e93d1cb0dd5
Merlin Webster <mwebster@witekio.com>
2020-04-27 17:37:34 +0100

    Added a proxy serial console fixture
    This fixture uses real SerialConsole object, but hooked up to a pseudo tty
    This allows the SerialConsole to behave as if it is talking to a real serial
    port. The input and output for the console is actually sent to a file descriptor
    the system. This can be written to and read from to emulate interactions with a
    real serial port.

--- generic/generic_fixtures.py
+++ generic/generic_fixtures.py
@@ -1,8 +1,21 @@
+import pty
+import os
+from collections import namedtuple
 from pytest import fixture
 from unittest.mock import MagicMock
-from farmcore import SoftPower
+from farmcore import SoftPower, SerialConsole
 from farmcore.mocks import ConsoleMock
 
+
+class OsFile:
+    def __init__(self, fd):
+        self.fd = fd
+
+    def read(self, n=None):
+        return os.read(self.fd, n or 10000)
+
+    def write(self, msg):
+        return os.write(self.fd, msg)
 
 @fixture
 def soft_power():
@@ -13,3 +26,30 @@ def soft_power():
         on_cmd='MOCK ON',
         off_cmd='MOCK OFF'
     )
+
+
+@fixture
+def serial_console_proxy():
+    # === Setup ===
+    master, slave = pty.openpty()
+
+    slave_device = os.ttyname(slave)
+
+    console = SerialConsole(
+        port=slave_device,
+        baud=115200 # Baud Doesn't really matter as virtual tty
+    )
+
+    SerialConsoleProxy = namedtuple('SerialConsoleProxy', ['proxy', 'console'])
+
+    # === Return Fixture ===
+    yield SerialConsoleProxy(OsFile(master), console)
+
+    # === Teardown ===
+    console.close()
+
+    for fd in [master, slave]:
+        try:
+            os.close(fd)
+        except OSError:
+            pass
