f2778136c5fa07cd11ca1cabe18271554dac15df
Merlin Webster <mwebster@witekio.com>
2020-04-28 19:14:44 +0100

    Added SerialConsole.send with match tests

--- generic/test_SerialConsole.py
+++ generic/test_SerialConsole.py
@@ -44,7 +44,6 @@ def test_SerialConsole_send_returns_tuple_length_2(serial_console_proxy):
 
     assert len(returned) == 2
 
-
 def test_SerialConsole_send_returns_data_when_recieve_arg_true(serial_console_proxy):
     msg = 'Bar'
 
@@ -58,3 +57,84 @@ def test_SerialConsole_send_returns_data_when_recieve_arg_true(serial_console_pr
 
     received, __ = async_result.get()
     assert received == msg
+
+
+def test_SerialConsole_send_returns_matched_when_match_available(serial_console_proxy):
+    before_match, to_match = 'Foo', 'Bar'
+    msg = before_match + to_match
+
+    async_result = nonblocking(serial_console_proxy.console.send,
+        match=to_match)
+
+    # Wait short time for function to start
+    time.sleep(0.1)
+
+    serial_console_proxy.proxy.write(msg, serial_console_proxy.console.encoding)
+
+    __, matched = async_result.get()
+    assert matched == to_match
+
+
+def test_SerialConsole_send_returns_received_when_match_available(serial_console_proxy):
+    before_match, to_match = 'Foo', 'Bar'
+    msg = before_match + to_match
+
+    async_result = nonblocking(serial_console_proxy.console.send,
+        match=to_match)
+
+    # Wait short time for function to start
+    time.sleep(0.1)
+
+    serial_console_proxy.proxy.write(msg, serial_console_proxy.console.encoding)
+
+    received, __ = async_result.get()
+    assert received == before_match
+
+
+def test_SerialConsole_send_returns_recieved_false_when_no_match_available(serial_console_proxy):
+    msg = 'FooBar'
+    wont_match = 'Baz'
+
+    async_result = nonblocking(serial_console_proxy.console.send,
+        match=wont_match)
+
+    # Wait short time for function to start
+    time.sleep(0.1)
+
+    serial_console_proxy.proxy.write(msg, serial_console_proxy.console.encoding)
+
+    __, matched = async_result.get()
+    assert matched is False
+
+
+def test_SerialConsole_send_returns_recieved_when_no_match_available(serial_console_proxy):
+    msg = 'FooBar'
+    wont_match = 'Baz'
+
+    async_result = nonblocking(serial_console_proxy.console.send,
+        match=wont_match)
+
+    # Wait short time for function to start
+    time.sleep(0.1)
+
+    serial_console_proxy.proxy.write(msg, serial_console_proxy.console.encoding)
+
+    received, __ = async_result.get()
+    assert received == msg
+
+
+def test_SerialConsole_send_matches_regex(serial_console_proxy):
+    msg = 'Hello World! 123FooBarBaz'
+    regex = '[0-3]+Foo'
+    expected_match='123Foo'
+
+    async_result = nonblocking(serial_console_proxy.console.send,
+        match=regex)
+
+    # Wait short time for function to start
+    time.sleep(0.1)
+
+    serial_console_proxy.proxy.write(msg, serial_console_proxy.console.encoding)
+
+    __, matched = async_result.get()
+    assert matched == expected_match
