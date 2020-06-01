e111a9fd6a5351b8b0e19fe16ef5e83152a60fc1
Merlin Webster <mwebster@witekio.com>
2020-06-01 12:13:03 +0100

    SoftPower: added a reboot_cmd option

--- farmcore/softpower.py
+++ farmcore/softpower.py
@@ -4,12 +4,13 @@ from .exceptions import PDUError
 
 class SoftPower(PowerBase):
     def __init__(self, console, on_cmd=None, off_cmd=None,
-            reboot_delay=None):
+            reboot_cmd=None, reboot_delay=None):
         assert isinstance(console, ConsoleBase)
 
         self.console = console
         self.on_cmd = on_cmd
         self.off_cmd = off_cmd
+        self.reboot_cmd = reboot_cmd
 
         PowerBase.__init__(self, reboot_delay)
 
@@ -26,3 +27,13 @@ class SoftPower(PowerBase):
             self.console.send(self.off_cmd)
         else:
             self.error('No off_cmd, cannot soft power off', PDUError)
+
+    def reboot(self):
+        if self.reboot_cmd:
+            self.log(f'{str(self)}: Rebooting...')
+            self.console.send(self.reboot_cmd)
+        elif self.off_cmd and self.on_cmd:
+            PowerBase.reboot(self)
+        else:
+            self.error('Need either reboot_cmd or both off_cmd and on_cmd '
+                'to soft reboot', PDUError)
