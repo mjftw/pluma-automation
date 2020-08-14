dcc2ba120738a68dfdd2ffb0fe42503480ae768d
Adrien Leravat <aleravat@witekio.com>
2020-08-13 20:25:21 -0700

    testsuite: add 'KernelModulesLoaded' test

--- farmtest/__init__.py
+++ farmtest/__init__.py
@@ -3,5 +3,5 @@ from .exceptions import *
 from .test import TestBase, TestCore, TestRunner
 from .unittest import deferred_function
 from .testcontroller import TestController
-from .shelltest import ShellTest
+from .shelltest import ShellTest, CommandRunner
 from .executabletest import ExecutableTest
---
+++ testsuite/kernel.py
@@ -0,0 +1,47 @@
+import math
+import os
+import re
+
+from farmtest import TestBase, TaskFailed, CommandRunner
+from farmcore.baseclasses import ConsoleBase
+
+
+class KernelModulesLoaded(TestBase):
+    def __init__(self, board, modules: list):
+        super().__init__(self)
+        self.board = board
+
+        if modules is None:
+            raise ValueError(f'"modules" cannot be empty')
+
+        if isinstance(modules, str):
+            modules = [modules]
+        elif not isinstance(modules, list):
+            raise ValueError(
+                f'"modules" must be a string or a list, but "{modules}" was provided')
+
+        self.modules = modules
+
+    def test_body(self):
+        console = self.board.console
+        if not console:
+            raise TaskFailed('No console available')
+
+        output = CommandRunner.run(test_name=str(
+            self), console=console, command='lsmod', timeout=3)
+
+        missing_modules = []
+        for module in self.modules:
+            found = False
+            # For some reason, re.MULTILINE fails to match
+            for line in output.splitlines():
+                if re.match(fr'^{module}\b', line):
+                    found = True
+                    break
+
+            if not found:
+                missing_modules.append(module)
+
+        if missing_modules:
+            raise TaskFailed(
+                f'The following kernel modules are not loaded: "{missing_modules}"')
