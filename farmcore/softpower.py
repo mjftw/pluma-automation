9417209cb21043a055997981ac7d9f0e554fa157
Adrien Leravat <aleravat@witekio.com>
2020-08-13 20:28:07 -0700

    SoftPower: default reboot command to most commonly available
    Power: minor improvements to error logging

--- farmcore/softpower.py
+++ farmcore/softpower.py
@@ -12,6 +12,9 @@ class SoftPower(PowerBase):
         self.off_cmd = off_cmd
         self.reboot_cmd = reboot_cmd
 
+        if not self.reboot_cmd and not self.on_cmd and not self.off_cmd:
+            self.reboot_cmd = 'reboot || shutdown -r now || systemctl reboot'
+
         PowerBase.__init__(self, reboot_delay)
 
     @PowerBase.on
@@ -19,14 +22,14 @@ class SoftPower(PowerBase):
         if self.on_cmd is not None:
             self.console.send(self.on_cmd)
         else:
-            self.error('No on_cmd, cannot soft power on', PDUError)
+            self.error('No "on_cmd", cannot soft power on', PDUError)
 
     @PowerBase.off
     def off(self):
         if self.off_cmd is not None:
             self.console.send(self.off_cmd)
         else:
-            self.error('No off_cmd, cannot soft power off', PDUError)
+            self.error('No "off_cmd", cannot soft power off', PDUError)
 
     def reboot(self):
         if self.reboot_cmd is not None:
@@ -35,5 +38,5 @@ class SoftPower(PowerBase):
         elif self.off_cmd and self.on_cmd:
             PowerBase.reboot(self)
         else:
-            self.error('Need either reboot_cmd or both off_cmd and on_cmd '
-                'to soft reboot', PDUError)
+            self.error('Need either "reboot_cmd" or "both off_cmd" and "on_cmd" '
+                       'to soft reboot', PDUError)
