42342076129be6866791736b5085117282d6a16e
Adrien Leravat <aleravat@witekio.com>
2020-08-15 20:55:56 -0700

    ShellTest: add 'runs_in_shell' attribute
    Used to know standard shell commands are expected to work, or not.
    This allows running shell commands for bootloaders for instance without
    causing a test error

--- farmtest/shelltest.py
+++ farmtest/shelltest.py
@@ -10,13 +10,14 @@ from farmcore import HostConsole
 class ShellTest(TestBase):
     shell_test_index = 0
 
-    def __init__(self, board, script, name=None, should_print=None, should_not_print=None, run_on_host=False, timeout=None):
+    def __init__(self, board, script, name=None, should_print=None, should_not_print=None, run_on_host=False, timeout=None,  runs_in_shell: bool = True):
         super().__init__(self)
         self.board = board
         self.should_print = should_print or []
         self.should_not_print = should_not_print or []
         self.run_on_host = run_on_host
         self.timeout = timeout if timeout is not None else 5
+        self.runs_in_shell = runs_in_shell
 
         self.scripts = script
         if not isinstance(self.scripts, list):
@@ -51,13 +52,14 @@ class ShellTest(TestBase):
 
     def run_command(self, console, script):
         CommandRunner.run(test_name=self._test_name, console=console, command=script,
-                          timeout=self.timeout, should_print=self.should_print, should_not_print=self.should_not_print)
+                          timeout=self.timeout, should_print=self.should_print, should_not_print=self.should_not_print,
+                          runs_in_shell=self.runs_in_shell)
 
 
 class CommandRunner():
     @staticmethod
     def run(test_name: str, console: ConsoleBase, command: str,
-            timeout: int = None, should_print: list = None, should_not_print: list = None) -> str:
+            timeout: int = None, should_print: list = None, should_not_print: list = None, runs_in_shell: bool = True) -> str:
         should_print = should_print or []
         should_not_print = should_not_print or []
         log = Logger()
@@ -104,24 +106,22 @@ class CommandRunner():
         else:
             log.log(f'{prefix}{os.linesep}' + sent_line + received_line)
 
-        retcode_command = 'echo retcode=$?'
-        retcode_received, matched = console.send_and_expect(
-            retcode_command, match='retcode=0')
+        if runs_in_shell:
+            retcode_command = 'echo retcode=$?'
+            retcode_received, matched = console.send_and_expect(
+                retcode_command, match='retcode=0')
 
-        if retcode_received.startswith(retcode_command):
-            retcode_received = retcode_received[len(retcode_command):]
+            if retcode_received.startswith(retcode_command):
+                retcode_received = retcode_received[len(retcode_command):]
 
-        if not matched:
-            error = f'{prefix} Command "{command}" returned with a non-zero exit code{os.linesep}' + \
-                sent_line + received_line + \
-                f'  Return code query output: {retcode_received}'
-            ansi_colors_regexp = re.compile(
-                r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
-            error = ansi_colors_regexp.sub('', error)
-            raise TaskFailed(error)
-
-        # Be sure nothing stays in the buffer
-        console.wait_for_quiet()
+            if not matched:
+                error = f'{prefix} Command "{command}" returned with a non-zero exit code{os.linesep}' + \
+                    sent_line + received_line + \
+                    f'  Return code query output: {retcode_received}'
+                ansi_colors_regexp = re.compile(
+                    r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
+                error = ansi_colors_regexp.sub('', error)
+                raise TaskFailed(error)
 
     @staticmethod
     def output_matches_pattern(patterns, output):
