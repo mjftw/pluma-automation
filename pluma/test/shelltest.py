e8a6d44b02702c8e3a44b3e4b1054099d3880028
Adrien Leravat <aleravat@witekio.com>
2020-09-19 10:31:43 -0700

    ShellTest: wait for command completion
    At an architecture level, split raw command and shell commands, to start
    reducing classes and methods responsabilities.

--- pluma/test/shelltest.py
+++ pluma/test/shelltest.py
@@ -5,12 +5,15 @@ from pluma.core.baseclasses import ConsoleBase, Logger
 from pluma import HostConsole, Board
 from .test import TestBase, TaskFailed
 
+log = Logger()
+
 
 class ShellTest(TestBase):
     shell_test_index = 0
 
-    def __init__(self, board: Board, script: str, name: str = None, should_print: list = None, should_not_print: list = None,
-                 run_on_host: bool = False, timeout: int = None, runs_in_shell: bool = True, login_automatically: bool = True):
+    def __init__(self, board: Board, script: str, name: str = None, should_print: list = None,
+                 should_not_print: list = None, run_on_host: bool = False, timeout: int = None,
+                 runs_in_shell: bool = True, login_automatically: bool = True):
         super().__init__(board)
         self.should_print = should_print or []
         self.should_not_print = should_not_print or []
@@ -52,26 +55,142 @@ class ShellTest(TestBase):
             self.run_command(console, script)
 
     def run_command(self, console, script):
-        CommandRunner.run(test_name=self._test_name, console=console, command=script,
-                          timeout=self.timeout, should_print=self.should_print, should_not_print=self.should_not_print,
-                          runs_in_shell=self.runs_in_shell)
+        if self.runs_in_shell:
+            output = CommandRunner.run(test_name=self._test_name, console=console,
+                                       command=script, timeout=self.timeout)
+        else:
+            output = CommandRunner.run_raw(test_name=self._test_name, console=console,
+                                           command=script, timeout=self.timeout)
+
+        if self.should_print or self.should_not_print:
+            CommandRunner.check_output(test_name=self._test_name, command=script, output=output,
+                                       should_print=self.should_print,
+                                       should_not_print=self.should_not_print)
 
 
 class CommandRunner():
     @staticmethod
-    def run(test_name: str, console: ConsoleBase, command: str,
-            timeout: int = None, should_print: list = None, should_not_print: list = None, runs_in_shell: bool = True):
+    def run(test_name: str, console: ConsoleBase, command: str, timeout: int = None):
+        '''Run a command in a Shell context'''
+        retcode_token = 'pluma-retcode='
+        command += " && echo pluma-retcode=$?"
+        output, matched = console.send_and_expect(
+            command, timeout=10, match=retcode_token+r'-?\d+')
+
+        if not matched:
+            CommandRunner.log_error(test_name=test_name, sent=command, output=output,
+                                    error='No response within timeout, or failed device'
+                                    ' failed to send return code. If this is not running'
+                                    ' in a shell, set "runs_in_shell" to "false".')
+
+        output = re.sub(re.escape(matched) + r'$', '', output)
+        retcode_match = re.search(retcode_token+r'(-?\d+)', matched)
+        if not retcode_match:
+            raise Exception('Failed to find return code value')
+
+        retcode = int(retcode_match.group(1))
+        if retcode == 0:
+            error = None
+        elif retcode is None:
+            error = 'failed to retrieve return code for command'
+        else:
+            error = f'returned with exit code {retcode}'
+
+        if error:
+            CommandRunner.log_error(test_name=test_name, sent=command, output=output,
+                                    error=f'Command "{command}" {error}')
+
+        output = CommandRunner.cleanup_command_output(command, output)
+
+        if not output:
+            CommandRunner.log_error(test_name=test_name, sent=command, output=output,
+                                    error='No response received after sending command')
+        else:
+            message = CommandRunner.format_command_log(sent=command, output=output)
+            log.log(message)
+
+        return output
+
+    @staticmethod
+    def run_raw(test_name: str, console: ConsoleBase, command: str, timeout: int = None):
+        '''Run a command with minimal assumptions regarding the context'''
+        output = console.send_and_read(command, timeout=timeout, quiet_time=timeout)
+        output = CommandRunner.cleanup_command_output(command, output)
+
+        if not output:
+            CommandRunner.log_error(test_name=test_name, sent=command, output=output,
+                                    error='No response received after sending command')
+        else:
+            message = CommandRunner.format_command_log(sent=command, output=output)
+            log.log(message)
+
+    @staticmethod
+    def log_error(test_name: str, sent: str, output: str, error: str,
+                  should_print=None, should_not_print=None):
+        message = f'Script test "{test_name}": {error}:{os.linesep}'
+        message += CommandRunner.format_command_log(sent=sent, output=output,
+                                                    should_print=should_print,
+                                                    should_not_print=should_not_print)
+        raise TaskFailed(message)
+
+    @staticmethod
+    def format_command_log(sent: str, output: str, should_print=None, should_not_print=None):
+        '''Return a formatted log output regarding a command and its output'''
+        formatted_output = ''
+        first_line = True
+        for line in output.splitlines():
+            if not first_line:
+                formatted_output += '            '
+            formatted_output += f'{line}{os.linesep}'
+            first_line = False
+
+        sent_line = f'  Sent:     $ {sent}{os.linesep}'
+        should_print_line = f'  Expected: {should_print}{os.linesep}'
+        should_not_print_line = f'  Errors:   {should_not_print}{os.linesep}'
+        output_line = f'  Output:   {formatted_output}'
+
+        message = sent_line
+        if should_print:
+            message += should_print_line
+        if should_not_print:
+            message += should_not_print_line
+
+        message += output_line
+        return message
+
+    @staticmethod
+    def check_output(test_name: str, command: str, output: str,
+                     should_print: list, should_not_print: list):
+        '''Check that command output includes expected content, and no error content'''
         should_print = should_print or []
         should_not_print = should_not_print or []
-        log = Logger()
 
-        output = console.send_and_read(command, timeout=timeout, quiet_time=timeout)
+        if should_not_print:
+            if not CommandRunner.output_matches_pattern(should_not_print, output):
+                CommandRunner.log_error(test_name=test_name, sent=command, output=output,
+                                        should_not_print=should_not_print,
+                                        error='Response matched error condition')
 
+        if should_print:
+            pattern_matched = CommandRunner.output_matches_pattern(should_print, output)
+
+            if not pattern_matched:
+                CommandRunner.log_error(test_name=test_name, sent=command, output=output,
+                                        should_print=should_print,
+                                        error='Response did not match expected')
+            else:
+                message = CommandRunner.format_command_log(sent=command, output=output,
+                                                           should_print=should_print)
+                log.log(message)
+
+    @staticmethod
+    def cleanup_command_output(command: str, output: str) -> str:
+        '''Return the output of a command only, stripping prompts'''
         command_end = None
         # Look for 2 instances of the command max, as it may echo twice,
         # once as a console echo, and once after the prompt.
         i = 0
-        for match in re.finditer(command, output):
+        for match in re.finditer(re.escape(command), output):
             i += 1
             if i > 2:
                 break
@@ -81,60 +200,7 @@ class CommandRunner():
         if command_end:
             output = output[command_end:]
 
-        output = output.strip()
-        prefix = f'Script test "{test_name}":'
-        if not output:
-            raise TaskFailed(
-                f'{prefix} No response received after sending command')
-
-        formatted_output = ''
-        first_line = True
-        for line in output.splitlines():
-            if not first_line:
-                formatted_output += '            '
-            formatted_output += f'{line}{os.linesep}'
-            first_line = False
-
-        sent_line = f'  Sent:     $ {command}{os.linesep}'
-        should_print_line = f'  Expected: {should_print}{os.linesep}'
-        should_not_print_line = f'  Errors:   {should_not_print}{os.linesep}'
-        output_line = f'  Output:   {formatted_output}'
-
-        if CommandRunner.output_matches_pattern(should_not_print, output):
-            raise TaskFailed(
-                f'{prefix} Response matched error condition:{os.linesep}' +
-                sent_line + should_not_print_line + output_line)
-
-        if should_print:
-            response_valid = CommandRunner.output_matches_pattern(
-                should_print, output)
-
-            if not response_valid:
-                raise TaskFailed(
-                    f'{prefix} Response did not match expected:{os.linesep}' +
-                    sent_line + should_print_line + output_line)
-            else:
-                log.log(
-                    f'{prefix} Matching response found:{os.linesep}' +
-                    sent_line + should_print_line + output_line)
-        else:
-            log.log(f'{prefix}{os.linesep}' + sent_line + output_line)
-
-        if runs_in_shell:
-            retcode = CommandRunner.query_return_code(console)
-            if retcode == 0:
-                error = None
-            elif retcode is None:
-                error = 'failed to retrieve return code for command'
-            else:
-                error = f'returned with exit code {retcode}'
-
-            if error:
-                error = f'{prefix} Command "{command}" {error}{os.linesep}' + \
-                    sent_line + output_line
-                raise TaskFailed(error)
-
-        return output
+        return output.strip()
 
     @staticmethod
     def query_return_code(console: ConsoleBase):
