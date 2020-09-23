3724f45fa51edccdb21ccb91e71141afcaddbb8b
Merlin Webster <mwebster@witekio.com>
2020-09-23 13:03:36 +0100

    Testsuite: Add file size test

--- pluma/plugins/testsuite/filesystem.py
+++ pluma/plugins/testsuite/filesystem.py
@@ -3,61 +3,86 @@ from pluma import Board
 
 
 class FileExists(ShellTest):
-    def __init__(self, board: Board, path: str):
-        super().__init__(board, script=f'[ -e "{path}" ]')
+    def __init__(self, board: Board, path: str, run_on_host: bool = False):
+        super().__init__(board, script=f'[ -e "{path}" ]', runs_in_shell=True,
+                         run_on_host=run_on_host)
 
 
 class FileIsRegular(ShellTest):
-    def __init__(self, board: Board, path: str):
-        super().__init__(board, script=f'[ -f "{path}" ]')
+    def __init__(self, board: Board, path: str, run_on_host: bool = False):
+        super().__init__(board, script=f'[ -f "{path}" ]', runs_in_shell=True,
+                         run_on_host=run_on_host)
 
 
 class FileIsDir(ShellTest):
-    def __init__(self, board: Board, path: str):
-        super().__init__(board, script=f'[ -d "{path}" ]')
+    def __init__(self, board: Board, path: str, run_on_host: bool = False):
+        super().__init__(board, script=f'[ -d "{path}" ]', runs_in_shell=True,
+                         run_on_host=run_on_host)
 
 
 class FileIsNotEmpty(ShellTest):
-    def __init__(self, board: Board, path: str):
-        super().__init__(board, script=f'[ -s "{path}" ]')
+    def __init__(self, board: Board, path: str, run_on_host: bool = False):
+        super().__init__(board, script=f'[ -s "{path}" ]', runs_in_shell=True,
+                         run_on_host=run_on_host)
 
 
 class FileIsEmpty(ShellTest):
-    def __init__(self, board: Board, path: str):
-        super().__init__(board, script=f'[ ! -s "{path}" ]')
+    def __init__(self, board: Board, path: str, run_on_host: bool = False):
+        super().__init__(board, script=f'[ ! -s "{path}" ]', runs_in_shell=True,
+                         run_on_host=run_on_host)
 
 
 class FileIsCharDevice(ShellTest):
-    def __init__(self, board: Board, path: str):
-        super().__init__(board, script=f'[ -c "{path}" ]')
+    def __init__(self, board: Board, path: str, run_on_host: bool = False):
+        super().__init__(board, script=f'[ -c "{path}" ]', runs_in_shell=True,
+                         run_on_host=run_on_host)
 
 
 class FileIsBlockDevice(ShellTest):
-    def __init__(self, board: Board, path: str):
-        super().__init__(board, script=f'[ -b "{path}" ]')
+    def __init__(self, board: Board, path: str, run_on_host: bool = False):
+        super().__init__(board, script=f'[ -b "{path}" ]', runs_in_shell=True,
+                         run_on_host=run_on_host)
 
 
 class FileIsSymlink(ShellTest):
-    def __init__(self, board: Board, path: str):
-        super().__init__(board, script=f'[ -h "{path}" ]')
+    def __init__(self, board: Board, path: str, run_on_host: bool = False):
+        super().__init__(board, script=f'[ -h "{path}" ]', runs_in_shell=True,
+                         run_on_host=run_on_host)
 
 
 class FileIsSocket(ShellTest):
-    def __init__(self, board: Board, path: str):
-        super().__init__(board, script=f'[ -S "{path}" ]')
+    def __init__(self, board: Board, path: str, run_on_host: bool = False):
+        super().__init__(board, script=f'[ -S "{path}" ]', runs_in_shell=True,
+                         run_on_host=run_on_host)
 
 
 class FileIsReadable(ShellTest):
-    def __init__(self, board: Board, path: str):
-        super().__init__(board, script=f'[ -r "{path}" ]')
+    def __init__(self, board: Board, path: str, run_on_host: bool = False):
+        super().__init__(board, script=f'[ -r "{path}" ]', runs_in_shell=True,
+                         run_on_host=run_on_host)
 
 
 class FileIsWritable(ShellTest):
-    def __init__(self, board: Board, path: str):
-        super().__init__(board, script=f'[ -w "{path}" ]')
+    def __init__(self, board: Board, path: str, run_on_host: bool = False):
+        super().__init__(board, script=f'[ -w "{path}" ]', runs_in_shell=True,
+                         run_on_host=run_on_host)
 
 
 class FileIsExecutable(ShellTest):
-    def __init__(self, board: Board, path: str):
-        super().__init__(board, script=f'[ -x "{path}" ]')
+    def __init__(self, board: Board, path: str, run_on_host: bool = False):
+        super().__init__(board, script=f'[ -x "{path}" ]', runs_in_shell=True,
+                         run_on_host=run_on_host)
 
+
+class CheckFileSize(ShellTest):
+    def __init__(self, board: Board, path: str, min: str = None, max: str = None,
+                 run_on_host: bool = False):
+        conditions = []
+        if min:
+            conditions.append(f'[ $(stat --format=%s "{path}") -ge {min} ]')
+        if max:
+            conditions.append(f'[ $(stat --format=%s "{path}") -le {max} ]')
+
+        script = ' && '.join(conditions)
+
+        super().__init__(board, script=script, runs_in_shell=True, run_on_host=run_on_host)
