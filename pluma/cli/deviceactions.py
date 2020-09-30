f0965790a57d7e329a384a0218b22202cacd0285
Adrien Leravat <aleravat@witekio.com>
2020-09-29 21:18:53 -0700

    testsuite: define base class for manual action and test

--- pluma/cli/deviceactions.py
+++ pluma/cli/deviceactions.py
@@ -104,33 +104,10 @@ class CopyToDeviceAction(DeviceActionBase):
         self.board.console.copy_to_target(self.file, self.destination, self.timeout)
 
 
-@DeviceActionRegistry.register('manual_action')
-class ManualDeviceAction(DeviceActionBase):
-    '''Print a message, and wait for the user to press ENTER'''
+class ManualDeviceActionBase(DeviceActionBase):
+    '''Base class for manual tests'''
 
     def __init__(self, board: Board, message: str, name: str = None):
-        super().__init__(board)
-        if name:
-            self._test_name += f'[{name}]'
-        self.message = message
-
-        if not sys.__stdin__.isatty():
-            raise ValueError('Non-interactive shell detected: '
-                             'Manual actions must be used in an interactive shell')
-
-    def execute(self):
-        log.log(f'{os.linesep}Manual action:'
-                f'{os.linesep}  > {self.message}'
-                f'{os.linesep}Press ENTER when done',
-                level=LogLevel.INFO, bypass_hold=True, newline=False)
-        input()
-
-
-@DeviceActionRegistry.register('manual_test')
-class ManualTestDeviceAction(DeviceActionBase):
-    '''Print a message, expected behavior, and wait for user's feedback'''
-
-    def __init__(self, board: Board, message: str, expected: str, name: str = None):
         super().__init__(board)
         if name:
             self._test_name += f'[{name}]'
@@ -140,6 +117,34 @@ class ManualTestDeviceAction(DeviceActionBase):
         if not sys.__stdin__.isatty():
             raise ValueError('Non-interactive shell detected: '
                              'Manual actions must be used in an interactive shell')
+
+
+@DeviceActionRegistry.register('manual_action')
+class ManualDeviceAction(ManualDeviceActionBase):
+    '''Print a message, and wait for the user to press ENTER'''
+
+    def __init__(self, board: Board, message: str, name: str = None):
+        super().__init__(board, message=message, name=name)
+
+    def execute(self):
+        if self.manual_test_name:
+            test_title = f'Manual action "{self.manual_test_name}"'
+        else:
+            test_title = 'Manual action'
+
+        log.log(f'{os.linesep}{test_title}:'
+                f'{os.linesep}  > {self.message}'
+                f'{os.linesep}Press ENTER when done',
+                level=LogLevel.INFO, bypass_hold=True, newline=False)
+        input()
+
+
+@DeviceActionRegistry.register('manual_test')
+class ManualTestDeviceAction(ManualDeviceActionBase):
+    '''Print a message, expected behavior, and wait for user's feedback'''
+
+    def __init__(self, board: Board, message: str, expected: str, name: str = None):
+        super().__init__(board, message=message, name=name)
 
     def execute(self):
         entered = None
@@ -157,11 +162,9 @@ class ManualTestDeviceAction(DeviceActionBase):
                     level=LogLevel.INFO, bypass_hold=True, newline=False)
             entered = input().lower()
 
-        log.log(f'The user entered: "{entered}"',
-                level=LogLevel.INFO, bypass_hold=True)
-
-        log.log(f'Comments: ', level=LogLevel.INFO,
-                bypass_hold=True, newline=False)
+        log.log(f'The user entered: "{entered}"', level=LogLevel.INFO,
+                bypass_hold=True)
+        log.log('Comments: ', level=LogLevel.INFO, bypass_hold=True, newline=False)
         comments = input()
         log.log(f'The user entered: "{comments}"',
                 level=LogLevel.INFO, bypass_hold=True)
