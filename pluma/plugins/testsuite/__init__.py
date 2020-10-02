39d788356d9b30f27adf26277f0e8549ca5a7a27
Merlin Webster <mwebster@witekio.com>
2020-10-02 16:12:44 +0100

    include filesystem tests

--- pluma/plugins/testsuite/__init__.py
+++ pluma/plugins/testsuite/__init__.py
@@ -1,4 +1,18 @@
 from .kernel import KernelModulesLoaded
 from .memory import MemorySize
 from .networking import RespondsToPing, IperfBandwidth
-from . import filesystem
+from .filesystem import (
+    FileExists,
+    FileIsRegular,
+    FileIsDir,
+    FileIsNotEmpty,
+    FileIsEmpty,
+    FileIsCharDevice,
+    FileIsBlockDevice,
+    FileIsSymlink,
+    FileIsSocket,
+    FileIsReadable,
+    FileIsWritable,
+    FileIsExecutable,
+    CheckFileSize
+)
