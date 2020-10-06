1db2b605b00f97c5921ce2c559a57de7ca5738c7
Merlin Webster <mwebster@witekio.com>
2020-10-06 10:49:45 +0100

    Upgrade run_tests.sh arg parsing to fix "make test-coverage"

--- tests/scripts/run_tests.sh
+++ tests/scripts/run_tests.sh
@@ -2,6 +2,28 @@
 set -eu
 this_dir="$(dirname $0)"
 
+# Default argument values
+COVERAGE=0
+
+# Get arguments
+POSITIONAL=()
+while [[ $# -gt 0 ]]; do
+    key="$1"
+
+    case $key in
+        --coverage)
+        COVERAGE=1
+        shift # past argument
+        ;;
+        *)    # unknown option
+        POSITIONAL+=("$1") # save it in an array for later
+        shift # past argument
+        ;;
+    esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+
 test_dir="$(readlink -f $this_dir/..)"
 if [ "$#" -ge 1 ]; then
     test_dir="$(readlink -f $this_dir/../$1)"
@@ -19,7 +41,7 @@ fi
 test_command="-m pytest "$test_dir
 
 echo $@
-if [[ $@ == *--coverage* ]]; then
+if [ $COVERAGE -eq 1 ]; then
     echo "Running coverage"
     coverage run --source=pluma $test_command
     coverage xml
