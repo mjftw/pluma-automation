c0060d03df7e7103c16ba6139b0a15c83293a38c
Merlin Webster <mwebster@witekio.com>
2020-10-06 13:35:48 +0100

    Add Sphinx-apidoc to build api documentation

--- docs/.sphinx/apiguide.rst
+++ docs/.sphinx/apiguide.rst
@@ -3,18 +3,11 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to Pluma Automation's documentation!
+API Guide
 ============================================
 
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
 
-   apiguide
-
-Indices and tables
-==================
-
-* :ref:`genindex`
-* :ref:`modindex`
-* :ref:`search`
+   api/modules
--- docs/.sphinx/conf.py
+++ docs/.sphinx/conf.py
@@ -28,12 +28,19 @@ author = 'Witekio'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinxcontrib.apidoc',
     'sphinx.ext.autodoc',
     'sphinx.ext.coverage',
     'sphinx.ext.viewcode',
     'sphinx.ext.napoleon',
     'recommonmark'
 ]
+
+# API docs generation
+apidoc_module_dir = '../../pluma'
+apidoc_output_dir = 'api'
+# apidoc_excluded_paths = ['tests']
+apidoc_separate_modules = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
