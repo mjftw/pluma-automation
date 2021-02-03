96f833531b9f0db1da82072b634f473c9bd6bc9d
Adrien Leravat <aleravat@witekio.com>
2021-02-03 11:03:44 -0800

    SoftPower: unit test reboot method coverage

--- tests/generic/core/test_SoftPower.py
+++ tests/generic/core/test_SoftPower.py
@@ -1,22 +1,30 @@
-def test_SoftPower_on_calls_console_send(soft_power):
-    soft_power.on()
-
-    soft_power.console.send.assert_called()
+from unittest.mock import patch
 
 
 def test_SoftPower_on_calls_console_send_with_correct_cmd(soft_power):
+    cmd = 'myon'
+    soft_power.on_cmd = cmd
     soft_power.on()
-
-    soft_power.console.send.assert_called_with(soft_power.on_cmd)
+    soft_power.console.send.assert_called_with(cmd)
 
 
 def test_SoftPower_off_calls_console_send_with_correct_cmd(soft_power):
+    cmd = 'myoff'
+    soft_power.off_cmd = 'myoff'
     soft_power.off()
+    soft_power.console.send.assert_called_with(cmd)
 
-    soft_power.console.send.assert_called_with(soft_power.off_cmd)
+
+def test_SoftPower_reboot_calls_console_send_with_correct_cmd(soft_power):
+    cmd = 'myreboot'
+    soft_power.reboot_cmd = cmd
+    soft_power.reboot()
+    soft_power.console.send.assert_called_with(cmd)
 
 
-def test_SoftPower_off_calls_console_send(soft_power):
-    soft_power.off()
-
-    soft_power.console.send.assert_called()
+@patch('pluma.core.baseclasses.PowerBase.reboot')
+def test_SoftPower_reboot_calls_reboot_if_no_reboot_command(mock_base_reboot,
+                                                            soft_power):
+    soft_power.reboot_cmd = None
+    soft_power.reboot()
+    mock_base_reboot.assert_called_once()
